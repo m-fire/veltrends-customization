133a545f26253d1bf34c7696f30fa4756a10124f
countless-exercise <truestarmello@gmail.com>
2022-09-29 10:48:58 +0900

    config test: 라우트 URI Tag 테스트: 이 설정으로 카테고리분류 가능

--- 1-back-end-server/src/routes/api/auth/index.ts
+++ 1-back-end-server/src/routes/api/auth/index.ts
@@ -4,12 +4,28 @@ import UserService from '../../../service/UserService.js'
 const authRoute: FastifyPluginAsync = async (fastify) => {
   const userService = UserService.getInstance()
 
-  fastify.get('/login', async () => {
-    return userService.login()
-  })
-  fastify.get('/register', async () => {
-    return userService.register()
-  })
+  fastify.get(
+    '/login',
+    {
+      schema: {
+        tags: ['auth', 'user'],
+      },
+    },
+    async () => {
+      return userService.login()
+    },
+  )
+  fastify.get(
+    '/register',
+    {
+      schema: {
+        tags: ['auth'],
+      },
+    },
+    async () => {
+      return userService.register()
+    },
+  )
 }
 
 export default authRoute
