2f7507a998d682a1e05ef5e9df30494aada98fd1
countless-exercise <truestarmello@gmail.com>
2022-10-01 15:12:53 +0900

    refactor docs: auth/schema -> common/../common-schema: 공통스키마 분리/이름변경

---
+++ 1-back-end-server/src/common/schema/common-schema.ts
@@ -0,0 +1,48 @@
+import { SchemaStruct } from '../config/fastify/types.js'
+
+// Routes
+
+export const SCHEMA_USER: SchemaStruct = {
+  type: 'object',
+  properties: {
+    id: { type: 'number' },
+    username: { type: 'string' },
+  },
+}
+
+// Errors
+
+export const SCHEMA_APP_ERROR: SchemaStruct = {
+  type: 'object',
+  properties: {
+    type: { type: 'string' },
+    message: { type: 'string' },
+    statusCode: { type: 'number' },
+  },
+}
+
+// Requests
+
+export const REQUEST_BODY_USERINFO: SchemaStruct = {
+  type: 'object',
+  properties: {
+    username: { type: 'string' },
+    password: { type: 'string' },
+  },
+}
+
+// Responses
+
+export const RESPONSE_200_AUTH_RESULT: SchemaStruct = {
+  type: 'object',
+  properties: {
+    token: {
+      type: 'object',
+      properties: {
+        accessToken: { type: 'string' },
+        refreshToken: { type: 'string' },
+      },
+    },
+    user: SCHEMA_USER,
+  },
+}
--- 1-back-end-server/src/routes/api/auth/schema.ts
+++ 1-back-end-server/src/routes/api/auth/schema.ts
@@ -1,44 +1,19 @@
 import { FastifySchema } from 'fastify'
-import { RES_ERROR_COMMON } from '../../../common/error/schema.js'
-import { SchemaStruct } from '../../../common/config/fastify/types.js'
+import {
+  REQUEST_BODY_USERINFO,
+  RESPONSE_200_AUTH_RESULT,
+  SCHEMA_APP_ERROR,
+} from '../../../common/schema/common-schema.js'
 
 // { body, querystring, params, headers, response } 등
 // HTTP 통신에 필요한 파라미터 설정
 
-const REQ_BODY_USERINFO: SchemaStruct = {
-  type: 'object',
-  properties: {
-    username: { type: 'string' },
-    password: { type: 'string' },
-  },
-}
-
-export const RES_200_TOKEN_N_USER: SchemaStruct = {
-  type: 'object',
-  properties: {
-    token: {
-      type: 'object',
-      properties: {
-        accessToken: { type: 'string' },
-        refreshToken: { type: 'string' },
-      },
-    },
-    user: {
-      type: 'object',
-      properties: {
-        id: { type: 'number' },
-        username: { type: 'string' },
-      },
-    },
-  },
-}
-
 export const SCHEMA_REGISTER_POST: FastifySchema = {
-  body: REQ_BODY_USERINFO,
+  body: REQUEST_BODY_USERINFO,
   response: {
-    201: RES_200_TOKEN_N_USER,
+    201: RESPONSE_200_AUTH_RESULT,
     409: {
-      ...RES_ERROR_COMMON,
+      ...SCHEMA_APP_ERROR,
       example: {
         type: 'UserExistsError',
         statusCode: 409,
@@ -50,11 +25,11 @@ export const SCHEMA_REGISTER_POST: FastifySchema = {
 
 export const SCHEMA_LOGIN_POST: FastifySchema = {
   /* 주의!: get 방식은 body 가 포함될 경우, 애러발생 */
-  body: REQ_BODY_USERINFO,
+  body: REQUEST_BODY_USERINFO,
   response: {
-    200: RES_200_TOKEN_N_USER,
+    200: RESPONSE_200_AUTH_RESULT,
     401: {
-      ...RES_ERROR_COMMON,
+      ...SCHEMA_APP_ERROR,
       example: {
         type: 'AuthenticationError',
         statusCode: 401,
