5a9ab09ecb459375f837153a744905a0a4761be7
countless-exercise <truestarmello@gmail.com>
2022-10-02 01:34:17 +0900

    feat: AppError: 인증토큰(불포함|만료) 검증2: 조건별 ErrorPayload 제공 생성자로 수정

--- 1-back-end-server/src/common/error/AppError.ts
+++ 1-back-end-server/src/common/error/AppError.ts
@@ -9,7 +9,7 @@ interface ErrorInfo {
   statusCode: number
 }
 
-const statusCodeMap: Record<AppErrorType, ErrorInfo> = {
+const ERROR_INFO_BY_TYPE: Record<AppErrorType, ErrorInfo> = {
   UserExistsError: {
     message: 'User already exists',
     statusCode: 409,
@@ -18,23 +18,37 @@ const statusCodeMap: Record<AppErrorType, ErrorInfo> = {
     message: 'Invalid username password',
     statusCode: 401,
   },
-
   UnauthorizedError: {
     message: 'Unauthorized',
     statusCode: 401,
   },
-
   UnknownError: {
     message: 'Unknown error',
     statusCode: 500,
   },
 }
 
-export default class AppError extends Error {
-  public statusCode: number
+type ErrorPayloadOpt<K extends AppErrorType> = K extends 'UserExistsError'
+  ? undefined
+  : K extends 'AuthenticationError'
+  ? undefined
+  : K extends 'UnauthorizedError'
+  ? {
+      isExpiredToken: boolean
+    }
+  : K extends 'UnknownError'
+  ? undefined
+  : never
 
-  constructor(public type: AppErrorType) {
-    const info = statusCodeMap[type]
+// Common Error
+
+export default class AppError<
+  K extends AppErrorType = AppErrorType,
+> extends Error {
+  public readonly statusCode: number
+
+  constructor(public readonly type: K, public payload?: ErrorPayloadOpt<K>) {
+    const info = ERROR_INFO_BY_TYPE[type]
     super(info.message)
     this.statusCode = info.statusCode
   }
