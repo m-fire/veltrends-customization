7739636bbacea095992efe699a464832d7532b1b
countless-exercise <truestarmello@gmail.com>
2022-10-02 21:38:36 +0900

    refactor: 중복인증로직 추출/ 새 플러그인 작성: me/index-> secure-access-plugin

--- 1-back-end-server/src/routes/api/me/index.ts
+++ 1-back-end-server/src/routes/api/me/index.ts
@@ -4,19 +4,6 @@ import AppError from '../../../common/error/AppError.js'
 
 const meRoute: FastifyPluginAsync = async (fastify) => {
   fastify.get('/', { schema: SCHEMA_ME_GET }, async (request) => {
-    /* Todo: 인증된 사용자의 access 마다 인증을 어떤방식으로 처리 할 것인가? */
-    // 1. 쿠키/해더 에 포함된 token 값으로 처리-> X: 접속마다 토큰파싱을 해야함.
-    // 2. 글로벌 플러그인 적용: `fastify-plugin`-> common/fastify/plugin/auth-plugin.ts
-
-    if (request.isExpiredToken)
-      throw new AppError('UnauthorizedError', {
-        isExpiredToken: true, // 만료된 토큰이 들어오면, 애러에 만료상태 설정
-      })
-    if (!request.user)
-      throw new AppError('UnauthorizedError', {
-        isExpiredToken: false, // 인증정보가 없다면 사용자의 만료토큰 비활성
-      })
-
     return request.user
   })
 }
