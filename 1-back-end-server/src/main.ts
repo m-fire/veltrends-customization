a79a8965cf39504669d7d76b782f800aff8e07cf
countless-exercise <truestarmello@gmail.com>
2022-10-02 17:14:42 +0900

    config: 플러그인 등록: fastifyCookie-> server

--- 1-back-end-server/src/main.ts
+++ 1-back-end-server/src/main.ts
@@ -1,5 +1,6 @@
 import fastify, { FastifyInstance } from 'fastify'
 import fastifySwagger from '@fastify/swagger'
+import fastifyCookie from '@fastify/cookie'
 import routes from './routes/index.js'
 import { swaggerOptions } from './common/config/fastify/swagger.js'
 import AppError from './common/error/AppError.js'
@@ -14,6 +15,7 @@ const server: FastifyInstance = fastify({
   // js 파일 root 에 `await`사용은 Node16 부터 지원.
   // 즉, tsconfig 의 모듈버전을 ES2022로 맞출 시 애러없이 실행가능.
   await server.register(fastifySwagger, swaggerOptions)
+  server.register(fastifyCookie)
   // Todo: fastify 4.? 하위버전에서는 routes 전에 등록해야 플러그인 로직이 정상동작!
   server.register(authPlugin) // 전역에서 인증사용자 인증처리 플러그인
   server.register(routes)
