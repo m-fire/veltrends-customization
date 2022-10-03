1ec0250d177767e549a88f1aaa3a54f590e200b0
countless-exercise <truestarmello@gmail.com>
2022-10-04 04:38:23 +0900

    refactor: UserService 에서 TokenService 추출

---
+++ 1-back-end-server/src/service/TokenService.ts
@@ -0,0 +1,46 @@
+import { User } from '@prisma/client'
+import db from '../common/config/prisma/db-client.js'
+import {
+  generateToken,
+  RefreshTokenPayload,
+  validateToken,
+} from '../common/config/jwt/tokens.js'
+
+export default class TokenService {
+  private static instance: TokenService
+
+  private constructor() {}
+
+  static getInstance() {
+    if (!TokenService.instance) {
+      TokenService.instance = new TokenService()
+    }
+    return TokenService.instance
+  }
+
+  async generateTokens(user: User, oldTokenId?: number): Promise<AuthTokens> {
+    const { id: userId, username } = user
+    const tokenId =
+      oldTokenId ?? (await db.token.create({ data: { userId } })).id
+
+    const [accessToken, refreshToken] = await Promise.all([
+      generateToken({
+        type: 'access',
+        tokenId,
+        userId,
+        username,
+      }),
+      generateToken({
+        type: 'refresh',
+        tokenId,
+        rotationCounter: 1,
+      }),
+    ])
+    return { accessToken, refreshToken }
+  }
+}
+
+export interface AuthTokens {
+  accessToken: string
+  refreshToken: string
+}
--- 1-back-end-server/src/service/UserService.ts
+++ 1-back-end-server/src/service/UserService.ts
@@ -1,20 +1,14 @@
 import * as bcrypt from 'bcrypt'
 import { User } from '@prisma/client'
-import { Authentication } from '../routes/api/auth/types.js'
+import db from '../common/config/prisma/db-client.js'
+import { UserAuthInfo } from '../routes/api/auth/types.js'
 import AppError from '../common/error/AppError.js'
-import UserRepository from '../repository/UserRepository.js'
-import TokenRepository from '../repository/TokenRepository.js'
-import {
-  generateToken,
-  RefreshTokenPayload,
-  validateToken,
-} from '../common/config/jwt/tokens.js'
+import TokenService, { AuthTokens } from './TokenService.js'
 
 const SOLT_ROUNDS = 10
 
 class UserService {
-  private userRepository = UserRepository.getInstance()
-  private tokenRepository = TokenRepository.getInstance()
+  private tokenService = TokenService.getInstance()
 
   private constructor() {}
 
@@ -27,81 +21,63 @@ class UserService {
     return UserService.instance
   }
 
-  async register({ username, password }: Authentication) {
-    const exists = await this.userRepository.findUnique(username)
+  async register({
+    username,
+    password,
+  }: UserAuthInfo): Promise<AuthTokensAndUser> {
+    const exists = await db.user.findUnique({ where: { username } })
     if (exists) throw new AppError('UserExistsError')
 
-    // 패스워드 암호화 및 사용자저장
-
-    let passwordHash
+    // 패스워드 암호화 및 사용자 저장
     try {
       // Promise 는 어떤 애러가 발생할지 알 수 없기에, try catch 처리
-      passwordHash = await bcrypt.hash(password, SOLT_ROUNDS)
+      const passwordHash = await bcrypt.hash(password, SOLT_ROUNDS)
+      const newUser = await db.user.create({ data: { username, passwordHash } })
+      const tokens = await this.tokenService.generateTokens(newUser)
+
+      // console.log(`UserService.register() newUser, tokens:`, newUser, tokens)
+      return { tokens, user: newUser }
     } catch (e) {
       throw new AppError('UnknownError')
     }
-
-    const user = await this.userRepository.save(username, passwordHash)
-    return await this.composeTokenAndUser(user)
-  }
-
-  async refreshToken(oldToken: string) {
-    try {
-      const decoded = await validateToken<RefreshTokenPayload>(oldToken)
-    } catch (e) {
-      throw new AppError('RefreshTokenError')
-    }
-  }
-
-  /**
-   * { 인증토큰, User }
-   */
-  private async composeTokenAndUser(user: User) {
-    const { id: userId, username } = user
-    const token = await this.tokenRepository.save(userId)
-
-    const [accessToken, refreshToken] = await Promise.all([
-      generateToken({
-        type: 'access',
-        tokenId: token.id,
-        userId,
-        username,
-      }),
-      generateToken({
-        type: 'refresh',
-        tokenId: token.id,
-        rotationCounter: 1,
-      }),
-    ])
-    const tokens = { accessToken, refreshToken }
-
-    return { tokens, user }
   }
 
   /**
    * 사용자를 못찾거나 PW가 틀릴경우, 보안상 모두 동일한 `AuthenticationError` 발생시킨다.
    */
-  async login({ username, password }: Authentication) {
-    const user = await this.userRepository.findUnique(username)
+  async login({
+    username,
+    password,
+  }: UserAuthInfo): Promise<AuthTokensAndUser> {
+    const existsUser = await db.user.findUnique({ where: { username } })
     // Promise 는 어떤 애러가 발생할지 알 수 없기에, try catch 처리
     try {
-      // 로그인 정보 검증
-      // 1. 가입된 사용자가 없으면  애러처리
-      // 2. PW가 일치하지 않으면 애러처리
-      if (!user || !(await bcrypt.compare(password, user.passwordHash)))
+      // 1:가입된 사용자가 없거나 2:PW가 일치하지 않으면 애러
+      if (
+        !existsUser ||
+        !(await bcrypt.compare(password, existsUser.passwordHash))
+      )
         throw new AppError('AuthenticationError')
+
+      const tokens = await this.tokenService.generateTokens(existsUser)
+      // console.log(`UserService.login() existsUser, tokens:`, existsUser, tokens)
+      return { tokens, user: existsUser }
     } catch (e) {
       // AppError 일 경우, 그대로 re-throw
       if (AppError.equals(e)) throw e
       // 알수없는 애러
       throw new AppError('UnknownError')
     }
-    return await this.composeTokenAndUser(user)
   }
 
-  private validateAuthentication() {
-    console.log(validateToken)
+  async refreshToken(oldToken: string): Promise<AuthTokens> {
+    try {
+      const decoded = await validateToken<RefreshTokenPayload>(oldToken)
+    } catch (e) {
+      throw new AppError('RefreshTokenError')
+    }
   }
 }
-
 export default UserService
+
+type AuthTokensAndUser = { tokens: AuthTokens; user: User }
