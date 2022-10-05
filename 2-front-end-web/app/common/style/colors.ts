cb59bf4b86fabfe189c12b9d8f96df2c0a56bf1d
countless-exercise <truestarmello@gmail.com>
2022-10-05 21:51:18 +0900

    config colors: 기본색상 정의(primary secondary, grey) from figma-plan

---
+++ 2-front-end-web/app/common/style/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  primary1: '#FFA000', // rgb(255, 160, 0),
+  primary2: '#FFB333', // rgb(255, 179, 51),
+  primary3: '#FFC159', // rgb(255, 193, 89),
+  primary4: '#FFD48C', // rgb(255, 212, 140),
+  primary5: '#FFE7BF', // rgb(255, 231, 191),
+  primary6: '#FFF1D9', // rgb(255, 241, 217),
+
+  secondary1: '#FF5601', // rgb(255, 86, 1),
+  secondary2: '#FF8041', // rgb(255, 128, 65),
+  secondary3: '#FFA273', // rgb(255, 162, 115),
+  secondary4: '#FFC7AB', // rgb(255, 199, 171),
+  secondary5: '#FFD8C4', // rgb(255, 216, 196),
+  secondary6: '#FFEFE6', // rgb(255, 239, 230),
+
+  grey1: '#EBEBEB', // rgb(235, 235, 235),
+  grey2: '#CCC', // rgb(204, 204, 204),
+  grey3: '#999', // rgb(153, 153, 153),
+  grey4: '#525252', // rgb(82, 82, 82),
+  grey5: '#292929', // rgb(41, 41, 41),
+  grey6: '#141414', // rgb(20, 20, 20),
+}
