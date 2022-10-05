0edd385f4eb4154b02cfc94305001f181de12beb
countless-exercise <truestarmello@gmail.com>
2022-10-05 17:20:43 +0900

    add comp: 공통컴포넌트 Header 작성 | index 라우트에 마운트

---
+++ 2-front-end-web/app/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+// Common Block
+const Block = styled.header``;
+
+type HeaderProps = {};
+
+function Header({}: HeaderProps) {
+  return <Block>Block</Block>;
+}
+
+Header.defaultProps = {
+  // initial prop values
+} as HeaderProps;
+
+export default Header;
