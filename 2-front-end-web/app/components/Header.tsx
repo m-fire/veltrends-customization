95f56a79160d8059a25474665219a333e1e8c2be
countless-exercise <truestarmello@gmail.com>
2022-10-06 06:44:09 +0900

    feat Header: SvgLogoVeltrend 를 감싸는 Block 스타일링, 중앙배치

--- 2-front-end-web/app/components/Header.tsx
+++ 2-front-end-web/app/components/Header.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
+import { colors } from '~/common/style/colors'
+import SvgLogoVeltrend from '~/components/generate/LogoVeltrend'
 
 // Common Block
-const Block = styled.header``
+const Block = styled.header`
+  height: 56px;
+  border-bottom: 1px solid ${colors.grey1};
+  padding-left: 16px;
+  padding-right: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  svg {
+    color: ${colors.grey6};
+    width: 130px;
+    height: 30px;
+  }
+`
 
 type HeaderProps = {}
 
 function Header({}: HeaderProps) {
-  return <Block>Block</Block>
+  return (
+    <Block>
+      <SvgLogoVeltrend />
+    </Block>
+  )
 }
 
 Header.defaultProps = {
