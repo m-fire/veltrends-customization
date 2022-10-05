c69698257e10329074769a71da7b5b0606c5a7a5
countless-exercise <truestarmello@gmail.com>
2022-10-06 07:57:14 +0900

    add Route /register: register.tsx 파일명으로 URI 자동설정

---
+++ 2-front-end-web/app/routes/register.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+type registerProps = {}
+
+function Register({}: registerProps) {
+  return <>등록페이지</>
+}
+
+Register.defaultProps = {
+  // initial prop values
+} as registerProps
+
+export default Register
