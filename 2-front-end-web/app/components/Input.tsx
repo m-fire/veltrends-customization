8011f252a88835570929df1ee3f2f16b6d99ca23
countless-exercise <truestarmello@gmail.com>
2022-10-06 17:26:03 +0900

    feat: Input 아이탬 구성 3: input 스타일링

--- 2-front-end-web/app/components/Input.tsx
+++ 2-front-end-web/app/components/Input.tsx
@@ -1,5 +1,6 @@
 import React, { HTMLAttributes } from 'react'
 import styled from 'styled-components'
+import { colors } from '~/common/style/colors'
 
 export interface InputProps extends HTMLAttributes<HTMLInputElement> {}
 
@@ -14,4 +15,12 @@ export default Input
 
 // Inner Components
 
-const StyledInput = styled.input``
+const StyledInput = styled.input`
+  height: 48px;
+  border: 1px solid ${colors.grey2};
+  border-radius: 6px;
+  outline: none;
+  font-size: 16px;
+  padding-left: 16px;
+  padding-right: 16px;
+`
