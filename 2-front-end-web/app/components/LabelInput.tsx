1359fe32a47ec945f907e6d5729696713a592fe7
countless-exercise <truestarmello@gmail.com>
2022-10-06 17:24:48 +0900

    feat: LabelInput 컨테이너 구성 4: div, label 스타일링

--- 2-front-end-web/app/components/LabelInput.tsx
+++ 2-front-end-web/app/components/LabelInput.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Input, { InputProps } from '~/components/Input'
+import { colors } from '~/common/style/colors'
 
 interface LabelInputProps extends InputProps {
   label: string
@@ -20,4 +21,20 @@ export default LabelInput
 
 // Inner Components
 
-const Block = styled.div``
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  label {
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${colors.grey4};
+    font-weight: 600;
+    margin-bottom: 8px;
+  }
+
+  /* 부모컴포넌트에서 gap 속성값(16px) 설정 되었으므로 불필요함 */
+  // 주의: 구형브라우저 적용불가
+  //& + & {
+  //  margin-top: 16px;
+  //}
+`
