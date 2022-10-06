cff4180d1f94f22b16e32eea60e3e4cce680abab
countless-exercise <truestarmello@gmail.com>
2022-10-06 11:02:18 +0900

    feat: HeaderBackButton 구성 3: 버튼 Element 기본CSS 초기화 및 스타일링

--- 2-front-end-web/app/components/HeaderBackButton.tsx
+++ 2-front-end-web/app/components/HeaderBackButton.tsx
@@ -13,4 +13,16 @@ function HeaderBackButton({}: HeaderBackButtonProps) {
 }
 export default HeaderBackButton
 
-const IconButton = styled.button``
+const IconButton = styled.button`
+  /* 버튼 기본CSS 초기화 */
+  padding: 0;
+  border: none;
+  background: none;
+  /* Todo: 터치영역 늘리기: 아이콘위치 W/H 중심유지 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* 4방패딩 늘리고, 원위치에서 이동한 만큼 minus 마진으로 원복 */
+  padding: 8px;
+  margin-left: -8px;
+`
