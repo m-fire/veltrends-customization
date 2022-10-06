a0e5a7f368dd6003a5e22a67ec28913015b8a243
countless-exercise <truestarmello@gmail.com>
2022-10-06 14:14:36 +0900

    feat: LabelInput.tsx 컨테이너 구성: div based

---
+++ 2-front-end-web/app/components/LabelInput.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+type LabelInputProps = {}
+
+function LabelInput({}: LabelInputProps) {
+  return (
+    <>
+      <Block>{}</Block>
+    </>
+  )
+}
+export default LabelInput
+
+// Inner Components
+
+const Block = styled.div``
