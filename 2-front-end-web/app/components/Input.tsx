42fac01207ba3ea17722043331171b4619ad3f46
countless-exercise <truestarmello@gmail.com>
2022-10-06 14:14:56 +0900

    feat: Input 아이탬 구성 1: input based 및, React.HTMLAttributes<> based props

---
+++ 2-front-end-web/app/components/Input.tsx
@@ -0,0 +1,17 @@
+import React, { HTMLAttributes } from 'react'
+import styled from 'styled-components'
+
+interface InputProps extends HTMLAttributes<HTMLInputElement> {}
+
+function Input(props: InputProps) {
+  return (
+    <>
+      <div>{}</div>
+    </>
+  )
+}
+export default Input
+
+// Inner Components
+
+const StyledInput = styled.input``
