7e4742ea43e5bfc246cdd12ee343bf8fcdd3207b
countless-exercise <truestarmello@gmail.com>
2022-10-13 10:39:22 +0900

    feat: Footer 생성 및 라우트 index 배치

---
+++ 2-front-end-web/app/components/base/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type FooterProps = {}
+
+function Footer({}: FooterProps) {
+  return (
+    <>
+      <div>Footer</div>
+    </>
+  )
+}
+export default Footer
+
+// Inner Components
--- 2-front-end-web/app/routes/index.tsx
+++ 2-front-end-web/app/routes/index.tsx
@@ -1,9 +1,11 @@
 import Header from '~/components/base/Header'
+import Footer from '~/components/base/Footer'
 
 export default function Index() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
