cd1bf7375bb1880031db5fddcc858685119ba1e0
countless-exercise <truestarmello@gmail.com>
2022-10-13 15:52:20 +0900

    feat route/index.ts: 컨탠츠영역 임시 컴포넌트(Content) 체우기

--- 2-front-end-web/app/routes/index.tsx
+++ 2-front-end-web/app/routes/index.tsx
@@ -1,12 +1,20 @@
 import Header from '~/components/base/Header'
 import Footer from '~/components/base/Footer'
 import FullHeightPage from '~/components/system/FullHeightPage'
+import styled from 'styled-components'
 
 export default function Index() {
   return (
     <FullHeightPage>
       <Header />
+      <Content />
       <Footer />
     </FullHeightPage>
   )
 }
+
+// Inner Components
+
+const Content = styled.div`
+  flex: 1;
+`
