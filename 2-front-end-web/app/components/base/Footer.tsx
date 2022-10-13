39b0cd39cf4f1d211e5488dfb434f1bb6248a062
countless-exercise <truestarmello@gmail.com>
2022-10-13 17:55:30 +0900

    refactor: Footer, FooterTabItem Nav-> NavLink 및 스타일링 변경

--- 2-front-end-web/app/components/base/Footer.tsx
+++ 2-front-end-web/app/components/base/Footer.tsx
@@ -1,8 +1,7 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { colors } from '~/common/style/colors'
 import FooterTabItem from '~/components/base/FooterTabItem'
-import { useLocation } from '@remix-run/react'
 
 const paths = ['search', 'bookmarks', 'setting'] as const
 
@@ -13,37 +12,15 @@ function isValidPath(path: any): path is typeof paths[number] {
 type FooterProps = {}
 
 function Footer({}: FooterProps) {
-  const location = useLocation()
-
-  const currentPage = useMemo(() => {
-    const path = location.pathname.split('/')[1]
-    if (isValidPath(path)) {
-      return path
-    }
-    return 'home'
-  }, [location.pathname])
-
   return (
     <StyledFooter>
-      <FooterTabItem to="/" icon="home" isActive={currentPage === 'home'} />
-      <FooterTabItem
-        to="/search"
-        icon="search"
-        isActive={currentPage === 'search'}
-      />
+      <FooterTabItem icon="home" to="/" />
+      <FooterTabItem icon="search" to="/search" />
 
       <FooterTabItem icon="add" />
 
-      <FooterTabItem
-        to="/bookmarks"
-        icon="bookmarks"
-        isActive={currentPage === 'bookmarks'}
-      />
-      <FooterTabItem
-        to="/setting"
-        icon="setting"
-        isActive={currentPage === 'setting'}
-      />
+      <FooterTabItem icon="bookmarks" to="/bookmarks" />
+      <FooterTabItem icon="setting" to="/setting" />
     </StyledFooter>
   )
 }
--- 2-front-end-web/app/components/base/FooterTabItem.tsx
+++ 2-front-end-web/app/components/base/FooterTabItem.tsx
@@ -1,4 +1,5 @@
-import React, { createElement } from 'react'
+import React, { createElement, MouseEventHandler } from 'react'
+import { NavLink } from '@remix-run/react'
 import styled, { css } from 'styled-components'
 import { colors } from '~/common/style/colors'
 import {
@@ -8,7 +9,6 @@ import {
   Search,
   Setting,
 } from '~/components/generate/svg'
-import { Link } from '@remix-run/react'
 
 const iconMap = {
   home: Fire,
@@ -19,66 +19,83 @@ const iconMap = {
 }
 
 type FooterTabItemProps = {
-  icon: keyof typeof iconMap
-  isActive?: boolean
   to?: string
+  icon: keyof typeof iconMap
 }
 
-function FooterTabItem({ icon, isActive, to }: FooterTabItemProps) {
+function FooterTabItem({ to, icon }: FooterTabItemProps) {
   const iconEl = createElement(iconMap[icon])
+
+  /* render NavLink */
 
   if (to) {
     return (
-      <LinkItemRef to={to} isActive={isActive}>
+      <NavLinkItemRef
+        to={to}
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
         {iconEl}
-      </LinkItemRef>
+      </NavLinkItemRef>
     )
   }
 
-  return <ButtonItem isActive={isActive}>{iconEl}</ButtonItem>
+  /* render Button */
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.currentTarget.classList.toggle('active')
+  }
+  return <ButtonItem onClick={handleClick}>{iconEl}</ButtonItem>
 }
 export default FooterTabItem
 
 // Inner Components
 
-const sharedStyle = (isActive?: boolean) => css`
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  svg {
-    color: ${colors.grey5};
-    width: 24px;
-    height: 24px;
-    ${isActive &&
-    css`
-      color: ${colors.primary1};
-    `}
-  }
+const NavLinkItemRef = styled(NavLink)`
+  ${sharedStyle()}
 `
 
-const LinkItemRef = styled(Link)<Pick<FooterTabItemProps, 'isActive'>>`
-  ${({ isActive }) => sharedStyle(isActive)}
-`
-
-const decoShapeCircleStyle = (isActive?: boolean) => css`
-  &::after {
-    display: block;
-    position: absolute;
-    width: 44px;
-    height: 44px;
-    content: '';
-    border: 2px solid ${isActive ? colors.primary1 : colors.grey5};
-    border-radius: 999px;
-  }
-`
-const ButtonItem = styled.button<Pick<FooterTabItemProps, 'isActive'>>`
+const ButtonItem = styled.button`
   background: none;
   outline: none;
   border: none;
   position: relative;
-  ${({ isActive }) => [
-    ...sharedStyle(isActive),
-    ...decoShapeCircleStyle(isActive),
-  ]};
+  ${[...sharedStyle(), ...shapeDecoStyle()]};
+  &.active {
+    &::after {
+      border: 4px solid ${colors.primary5};
+    }
+  }
 `
+
+function sharedStyle() {
+  return css`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg {
+      color: ${colors.grey5};
+      width: 24px;
+      height: 24px;
+    }
+    &.active {
+      svg {
+        color: ${colors.primary1};
+      }
+    }
+  `
+}
+
+function shapeDecoStyle() {
+  return css`
+    &::after {
+      display: block;
+      position: absolute;
+      width: 44px;
+      height: 44px;
+      content: '';
+      border: 4px solid ${colors.grey1};
+      border-radius: 999px;
+    }
+  `
+}
