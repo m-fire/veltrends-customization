bbdf2d79f8eab94e3e645473515d1c434875acf5
countless-exercise <truestarmello@gmail.com>
2022-10-13 14:31:06 +0900

    feat FooterTabItem: 아이탬요소를 `to`속성 존재여부에 `Link`or`Button`분기 랜더링

--- 2-front-end-web/app/components/base/FooterTabItem.tsx
+++ 2-front-end-web/app/components/base/FooterTabItem.tsx
@@ -8,6 +8,7 @@ import {
   Search,
   Setting,
 } from '~/components/generate/svg'
+import { Link } from '@remix-run/react'
 
 const iconMap = {
   home: Fire,
@@ -20,21 +21,44 @@ const iconMap = {
 type FooterTabItemProps = {
   icon: keyof typeof iconMap
   isActive?: boolean
+  to?: string
 }
 
-function FooterTabItem({ icon, isActive }: FooterTabItemProps) {
+function FooterTabItem({ icon, isActive, to }: FooterTabItemProps) {
   const iconEl = createElement(iconMap[icon])
-  return (
-    <>
-      <Item isActive={isActive}>{iconEl}</Item>
-    </>
-  )
+
+  if (to) {
+    return (
+      <LinkItemRef to={to} isActive={isActive}>
+        {iconEl}
+      </LinkItemRef>
+    )
+  }
+
+  return <ButtonItem isActive={isActive}>{iconEl}</ButtonItem>
 }
 export default FooterTabItem
 
 // Inner Components
 
-const Item = styled.div<Pick<FooterTabItemProps, 'isActive'>>`
+const LinkItemRef = styled(Link)<Pick<FooterTabItemProps, 'isActive'>>`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  svg {
+    color: ${colors.grey5};
+    width: 24px;
+    height: 24px;
+    ${({ isActive }) =>
+      isActive &&
+      css`
+        color: ${colors.primary1};
+      `}
+  }
+`
+
+const ButtonItem = styled.button<Pick<FooterTabItemProps, 'isActive'>>`
   flex: 1;
   display: flex;
   align-items: center;
