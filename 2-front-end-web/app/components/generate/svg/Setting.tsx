d93c8b29a06bb406bb88407d868a09a609a22446
countless-exercise <truestarmello@gmail.com>
2022-10-13 13:11:59 +0900

    Fix svg: 내부코드에 가로/세로 고정사이즈 제거함으로 외부값을 상속받도록 변경

--- 2-front-end-web/app/components/generate/svg/Setting.tsx
+++ 2-front-end-web/app/components/generate/svg/Setting.tsx
@@ -3,8 +3,7 @@ import { SVGProps } from 'react'
 
 const SvgSetting = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width={16}
-    height={16}
+    viewBox="0 0 16 16"
     fill="currentColor"
     xmlns="http://www.w3.org/2000/svg"
     role="img"
