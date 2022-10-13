ebbe6386f564e8eb145f37d2885b58953e75a2f6
countless-exercise <truestarmello@gmail.com>
2022-10-13 10:41:54 +0900

    feat route/index.ts: 요소들이 세로폭 전체를 사용하도록 `FullHeightPage` root 적용

--- 2-front-end-web/app/routes/index.tsx
+++ 2-front-end-web/app/routes/index.tsx
@@ -1,11 +1,12 @@
 import Header from '~/components/base/Header'
 import Footer from '~/components/base/Footer'
+import FullHeightPage from '~/components/system/FullHeightPage'
 
 export default function Index() {
   return (
-    <>
+    <FullHeightPage>
       <Header />
       <Footer />
-    </>
+    </FullHeightPage>
   )
 }
