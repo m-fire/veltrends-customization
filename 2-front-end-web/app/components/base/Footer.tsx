8528cc66e5023dede0c6731c59a442bad7fc9f26
countless-exercise <truestarmello@gmail.com>
2022-10-13 15:38:52 +0900

    feat Footer: 탭아이탬 클릭시 변경될 `활성화`상태설정

--- 2-front-end-web/app/components/base/Footer.tsx
+++ 2-front-end-web/app/components/base/Footer.tsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import styled from 'styled-components'
 import { colors } from '~/common/style/colors'
 import FooterTabItem from '~/components/base/FooterTabItem'
+import { useLocation } from '@remix-run/react'
+
+const paths = ['search', 'bookmark', 'setting'] as const
+
+function isValidPath(path: any): path is typeof paths[number] {
+  return paths.includes(path)
+}
 
 type FooterProps = {}
 
 function Footer({}: FooterProps) {
+  const location = useLocation()
+
+  const currentPage = useMemo(() => {
+    const path = location.pathname.split('/')[1]
+    if (isValidPath(path)) {
+      return path
+    }
+    return 'home'
+  }, [location.pathname])
+
   return (
     <StyledFooter>
-      <FooterTabItem icon="home" isActive />
-      <FooterTabItem icon="search" />
+      <FooterTabItem isActive={currentPage === 'home'} />
+      <FooterTabItem isActive={currentPage === 'search'} />
+
       <FooterTabItem icon="add" />
-      <FooterTabItem icon="bookmark" />
-      <FooterTabItem icon="setting" />
+
+      <FooterTabItem isActive={currentPage === 'bookmark'} />
+      <FooterTabItem isActive={currentPage === 'setting'} />
     </StyledFooter>
   )
 }
