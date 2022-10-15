22153979b1d0f36f11a2c45f3d9fc301de3659a6
countless-exercise <truestarmello@gmail.com>
2022-10-16 08:58:32 +0900

    feat: Item 등록기능 구현 2: ITEMS_POST_SCHEMA 의 response 200, 401 타입정의

--- 1-back-end-server/src/routes/api/items/schema.ts
+++ 1-back-end-server/src/routes/api/items/schema.ts
@@ -1,5 +1,6 @@
 import { FastifySchema } from 'fastify'
 import { Type } from '@sinclair/typebox'
+import { createAppErrorSchema } from '../../../common/util/schema-util.js'
 
 export const ITEM_CREATE_SCHEMA = Type.Object({
   title: Type.String(),
@@ -8,7 +9,34 @@ export const ITEM_CREATE_SCHEMA = Type.Object({
   tags: Type.Array(Type.String()),
 })
 
+export const ITEM_CREATE_RESULT_SCHEMA = Type.Object(
+  {
+    id: Type.String(),
+    title: Type.String(),
+    body: Type.String(),
+    link: Type.String(),
+    thumbnail: Type.Union([Type.String(), Type.Null()]),
+    createdAt: Type.String(),
+    updatedAt: Type.String(),
+  },
+  {
+    example: {
+      id: 1,
+      title: 'test_title',
+      body: 'test_body',
+      link: 'https://test.com/test',
+      thumbnail: null,
+      createdAt: '2022-10-15T23:16:21.901Z',
+      updatedAt: '2022-10-15T23:16:21.901Z',
+    },
+  },
+)
+
 export const ITEMS_POST_SCHEMA: FastifySchema = {
   tags: ['item'],
   body: ITEM_CREATE_SCHEMA,
+  response: {
+    201: ITEM_CREATE_RESULT_SCHEMA,
+    401: createAppErrorSchema('AuthenticationError'),
+  },
 }
