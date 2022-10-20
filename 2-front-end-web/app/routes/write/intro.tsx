5ebdf2587fc408af316e0337000e1d4de89dd164
countless-exercise <truestarmello@gmail.com>
2022-10-20 09:56:13 +0900

    feat: 글쓰기 입력데이터처리 2-2: Remix `useFetcher` 를 통한 `submit` 핸들러 | Write 컨택스트 데이터활용 구현

--- 2-front-end-web/app/routes/write/intro.tsx
+++ 2-front-end-web/app/routes/write/intro.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useFetcher } from '@remix-run/react'
 import styled from 'styled-components'
 import LabelInput from '~/components/system/LabelInput'
 import BasicLayout from '~/components/layout/BasicLayout'
@@ -6,19 +7,30 @@ import WriteFormTemplate from '~/components/write/WriteFormTemplate'
 import LabelTextArea from '~/components/system/LabelTextArea'
 import { Authenticator } from '~/common/api/auth'
 import { createItem } from '~/common/api/items'
+import { useWriteContext } from '~/context/WriteContext'
 
 type IntroProps = {}
 
 function Intro({}: IntroProps) {
+  const {
+    state: { link },
+  } = useWriteContext()
   const initialForm = { title: '', body: '' }
   const [form, setForm] = useState(initialForm)
+
+  /* Remix submitting hooks */
+  const fetcher = useFetcher<typeof initialForm>()
 
   return (
     <BasicLayout title="뉴스 소개" hasBackButton>
       <WriteFormTemplate
         description="공유할 뉴스를 소개하세요."
         buttonText="등록하기"
-        onSubmit={(e) => {}}
+        onSubmit={(e) => {
+          e.preventDefault()
+          fetcher.submit({ link, ...form }, { method: 'post' })
+          console.log(`Intro.onSubmit() fetcher.submit()`)
+        }}
       >
         <Group>
           <LabelInput
