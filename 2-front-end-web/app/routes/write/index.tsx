918c4edf0d7159ee5c260e35a02400d003dfa124
countless-exercise <truestarmello@gmail.com>
2022-10-20 09:42:35 +0900

    feat: 글쓰기 입력데이터처리 1: `link` 처리: 사용자 입력상태를 State|Context 각각저장 및 다음페이지 라우트이동 구현

--- 2-front-end-web/app/routes/write/index.tsx
+++ 2-front-end-web/app/routes/write/index.tsx
@@ -1,4 +1,4 @@
-import { FormEventHandler } from 'react'
+import { ChangeEventHandler, FormEventHandler, useState } from 'react'
 import { useNavigate } from '@remix-run/react'
 import BasicLayout from '~/components/layout/BasicLayout'
 import WriteFormTemplate from '~/components/write/WriteFormTemplate'
@@ -8,12 +8,16 @@ import LabelInput from '~/components/system/LabelInput'
 function WriteLink() {
   const navigate = useNavigate()
   const { state, actions } = useWriteContext()
+  const [link, setLink] = useState(state.link)
 
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
-    const formData = new FormData(e.currentTarget)
-    actions.setLink(formData.get('url') as string)
+    actions.setLink(link)
     navigate('/write/intro')
+  }
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setLink(e.target.value)
   }
 
   return (
@@ -27,7 +31,9 @@ function WriteLink() {
           label="URL"
           placeholder="https://example.com"
           name="url"
+          value={link}
           defaultValue={state.link}
+          onChange={onChange}
         />
       </WriteFormTemplate>
       {/* <Button onClick={() => navigate('/write/intro')}>다음</Button> */}
