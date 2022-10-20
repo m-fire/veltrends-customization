ac69a8b62f7e1965afe57e2b889edea75b1ba1ad
countless-exercise <truestarmello@gmail.com>
2022-10-20 20:36:49 +0900

    feat: PublisherService.ts 작성: 엔티티 Publisher 매핑서비스

---
+++ 1-back-end-server/src/service/PublisherService.ts
@@ -0,0 +1,32 @@
+import { Publisher } from '@prisma/client'
+import db from '../common/config/prisma/db-client'
+
+export default class PublisherService {
+  private static instance: PublisherService
+
+  static getInstance() {
+    if (!PublisherService.instance) {
+      PublisherService.instance = new PublisherService()
+    }
+    return PublisherService.instance
+  }
+
+  private constructor() {}
+
+  async createPublisher({ domain, name, favicon }: GetPublisherParams) {
+    const exists = await db.publisher.findUnique({ where: { domain } })
+    if (exists) return exists
+
+    const publisher = await db.publisher.create({
+      data: { domain, name, favicon },
+    })
+
+    return publisher
+  }
+}
+
+interface GetPublisherParams {
+  domain: string
+  name: string
+  favicon?: string
+}
