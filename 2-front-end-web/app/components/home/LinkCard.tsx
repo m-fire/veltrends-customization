aacd84e942d2a58be4a42bd31607ee852bcd4bd8
countless-exercise <truestarmello@gmail.com>
2022-10-24 10:24:21 +0900

    feat: Tip!: text 기반 CSS class 이름은 버그를 유발한다: className="thumbnail"

--- 2-front-end-web/app/components/home/LinkCard.tsx
+++ 2-front-end-web/app/components/home/LinkCard.tsx
@@ -1,19 +1,30 @@
 import React, { ReactNode } from 'react'
 import styled from 'styled-components'
 import { Item } from '~/common/api/types'
+import { colors } from '~/common/style/colors'
 
 type LinkCardProps = {
   item: Item
 }
 
-function LinkCard({}: LinkCardProps) {
-  return <Block>LinkCadd Item</Block>
+function LinkCard({ item }: LinkCardProps) {
+  const { thumbnail, title } = item
+
+  return (
+    <Block>
+      {thumbnail ? (
+        <img className="thumbnail" src={thumbnail} alt={title} />
+      ) : null}
+    </Block>
+  )
 }
 export default LinkCard
 
 // Inner Components
 
-const Block = styled.section`
+const Block = styled.div`
   display: flex;
   flex-direction: column;
+  .thumbnail {
+  }
 `
