92e6e84f8a850d74a5ed88c6a894614ab4906b97
countless-exercise <truestarmello@gmail.com>
2022-10-27 02:54:09 +0900

    Fix common/util/Requests.parseUrlParams(): /api/items 에 쿼리스트링 적용이 안되던 이유 수정

--- 2-front-end-web/app/common/util/https.ts
+++ 2-front-end-web/app/common/util/https.ts
@@ -1,6 +1,6 @@
 export class Requests {
   static parseUrlParams<T>(url: string) {
-    const paramsMap = new URLSearchParams(url)
+    const paramsMap = new URLSearchParams(new URL(url).searchParams)
     const result = {} as any
     for (const [k, v] of paramsMap) {
       result[k] = v
