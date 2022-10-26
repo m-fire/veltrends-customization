012686c3ba56e6342434846167f4f2a57b6235f2
countless-exercise <truestarmello@gmail.com>
2022-10-27 01:21:23 +0900

    feat: 목록 무한스크롤 로딩구현 1: api/items getItemList(..) 재구현

--- 2-front-end-web/app/common/api/items.ts
+++ 2-front-end-web/app/common/api/items.ts
@@ -1,5 +1,6 @@
 import { client, URL_API_SERVER } from './client.js'
 import { Item, ItemListPagination } from './types.js'
+import qs from 'qs'
 
 const URL_ITEMS = URL_API_SERVER + '/api/items'
 
@@ -14,7 +15,14 @@ type CreateItemParams = {
   body: string
 }
 
-export async function getItemList() {
-  const response = await client.get<ItemListPagination>(URL_ITEMS)
+export async function getItemList(cursor?: number) {
+  const response = await client.get<ItemListPagination>(
+    URL_ITEMS.concat(
+      qs.stringify(
+        { cursor },
+        { addQueryPrefix: true }, // 쿼리스트링 구분자 `?` 추가
+      ),
+    ),
+  )
   return response.data
 }
