3836fefef02b3d018a8d559b50f5db9cf6a95e9b
countless-exercise <truestarmello@gmail.com>
2022-11-10 06:29:45 +0900

    Fix: BE 엔티티타입 수정: isLiked 속성위치 변경: ItemStatus-> Item

--- 2-front-end-web/app/common/api/types.ts
+++ 2-front-end-web/app/common/api/types.ts
@@ -1,16 +1,17 @@
+export interface User {
+  id: number
+  username: string
+}
+
 export type GenericListPagination<T> = {
   list: T[]
   totalCount: number
   pageInfo: PageInfo
 }
+
 export interface PageInfo {
   lastCursor: number | null
   hasNextPage: boolean
-}
-
-export interface User {
-  id: number
-  username: string
 }
 
 export interface Item {
@@ -24,12 +25,20 @@ export interface Item {
   author: string
   user: User
   publisher: Publisher
+  itemStatus: ItemStatus
+  isLiked: boolean
 }
+
 export interface Publisher {
   id: number
   name: string
   domain: string
   favicon: string | null
+}
+
+export interface ItemStatus {
+  id: number
+  likes: number
 }
 
 export type ItemListPagination = GenericListPagination<Item>
