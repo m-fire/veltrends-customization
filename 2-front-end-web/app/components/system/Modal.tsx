b07f5f5457974df2087939b3b82a45f12377f854
countless-exercise <truestarmello@gmail.com>
2022-11-14 05:07:20 +0900

    feat: Modal > Positioner & Block 구현: Overlay 위에 보여질 컨탠츠 배치용 내부컴포넌트

--- 2-front-end-web/app/components/system/Modal.tsx
+++ 2-front-end-web/app/components/system/Modal.tsx
@@ -1,13 +1,33 @@
 import React, { ReactNode } from 'react'
 import Overlay from '~/components/system/Overlay'
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 type ModalProps = {
   children?: ReactNode
 }
 
 function Modal({ children }: ModalProps) {
-  return <Overlay visible>{children}</Overlay>
+  return (
+    <>
+      <Overlay />
+      <Positioner>
+        <Block>{children}</Block>
+      </Positioner>
+    </>
+  )
 }
 export default Modal
 
 // Inner Components
+
+const Positioner = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const Block = styled(motion.div)`
+  background: white;
+`
