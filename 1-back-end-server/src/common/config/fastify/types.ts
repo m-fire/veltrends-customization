231e1051b760ba26d368a7905af4adb71b39a67c
countless-exercise <truestarmello@gmail.com>
2022-11-23 17:09:13 +0900

    feat: 라우트 API 의 응답(res) 타입지정을 위한 MappedType(ts) 를 구현 후, 불필요해서 주석처리함

--- 1-back-end-server/src/common/config/fastify/types.ts
+++ 1-back-end-server/src/common/config/fastify/types.ts
@@ -1,6 +1,6 @@
 import { FastifySchema } from 'fastify'
 import { RequestGenericInterface } from 'fastify/types/request'
-import { Static, TSchema } from '@sinclair/typebox'
+import { Static, TObject, TSchema } from '@sinclair/typebox'
 import { PAGINATION_OPTION_SCHEMA } from '../typebox/common-schema.js'
 
 export interface Pagination<T> {
@@ -25,4 +25,19 @@ interface FastifyRequestProps<SCHEMA extends FastifySchema>
     ? Static<SCHEMA['querystring']>
     : never
   Body: SCHEMA['body'] extends TSchema ? Static<SCHEMA['body']> : never
+  // Header: SCHEMA['headers'] extends TSchema ? Static<SCHEMA['headers']> : never
 }
+
+/* 라우트 API 의 응답(res) 타입지정을 위해 구현하였으나, 불필요해서 주석처리함
+   향후 쓸모있을 수 있다. */
+// export type RouteResponseCodeMap<T extends Record<string, FastifySchema>> = {
+//   [A in keyof T]: T[A]['response'] extends Record<number, TSchema>
+//     ? FastifyResponseCodeProps<T[A]['response']>
+//     : never
+// }
+//
+// type FastifyResponseCodeProps<Res extends Record<string, TSchema>> = {
+//   [Code in keyof Res]: Res[Code] extends TSchema
+//     ? Record<Code, Static<Res[Code]>>
+//     : never
+// }
--- 1-back-end-server/src/routes/api/items/comments/types.ts
+++ 1-back-end-server/src/routes/api/items/comments/types.ts
@@ -2,3 +2,7 @@ import { RouteRequestMap } from '../../../../common/config/fastify/types.js'
 import COMMENTS_SCHEMA from './schema.js'
 
 export type CommentsRequestMap = RouteRequestMap<typeof COMMENTS_SCHEMA>
+
+// export type CommentsResponseCodeMap = RouteResponseCodeMap<
+//   typeof COMMENTS_SCHEMA
+// >
