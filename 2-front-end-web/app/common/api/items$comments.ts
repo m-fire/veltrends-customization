4f46b2a97a8b0d358387825469aa7ea21a31378e
countless-exercise <truestarmello@gmail.com>
2022-11-24 23:10:41 +0900

    Fix API items$comments: 작성한 댓글을 받아오기 위한 `text`속성 추가

--- 2-front-end-web/app/common/api/items$comments.ts
+++ 2-front-end-web/app/common/api/items$comments.ts
@@ -4,9 +4,27 @@ import { URL_ITEMS } from '~/common/api/items'
 
 const URL_COMMENTS = '/comments'
 
+export async function createComment({ itemId, text }: CreateCommentParams) {
+  const response = await client.post<Comment>(
+    `${URL_ITEMS}/${itemId}${URL_COMMENTS}`,
+    {
+      itemId,
+      text,
+    },
+  )
+  return response.data
+}
+
 export async function getCommentList(itemId: number) {
   const response = await client.get<Comment[]>(
     `${URL_ITEMS}/${itemId}${URL_COMMENTS}`,
   )
   return response.data
+}
+
+// types
+
+type CreateCommentParams = {
+  itemId: number
+  text: string
 }
