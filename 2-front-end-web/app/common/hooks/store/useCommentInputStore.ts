59795148201f5aa7c27fb06c150699e4a18617a5
countless-exercise <truestarmello@gmail.com>
2022-11-25 16:51:27 +0900

    Fix useCommentInputStore: `zustand` 훅의 open 매서드에 parentCommentId 를 받도록 타입수정

--- 2-front-end-web/app/common/hooks/store/useCommentInputStore.ts
+++ 2-front-end-web/app/common/hooks/store/useCommentInputStore.ts
@@ -1,13 +1,18 @@
 import create from 'zustand'
 
-export const useCommentInputStore = create<CommentInputStoreState>((set) => ({
-  visible: false,
-  open: () => set((s) => ({ ...s, visible: true })),
-  close: () => set((s) => ({ ...s, visible: false })),
-}))
+export const useCommentInputStore = create<CommentInputStateAndActions>(
+  (set) => ({
+    visible: false,
+    parentCommentId: null,
+    open: (parentCommentId) =>
+      set((s) => ({ ...s, parentCommentId, visible: true })),
+    close: () => set((s) => ({ ...s, visible: false })),
+  }),
+)
 
-type CommentInputStoreState = {
+type CommentInputStateAndActions = {
   visible: boolean
-  open(): void
+  parentCommentId: number | null
+  open(parentCommentId: number | null): void
   close(): void
 }
