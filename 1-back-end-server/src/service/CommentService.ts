9e7d0dcca5229ea00a8d7a1122497307668c1953
countless-exercise <truestarmello@gmail.com>
2022-11-25 15:49:55 +0900

    Fix CommentService.createComment: 사용자언급 구분로직 수정

--- 1-back-end-server/src/service/CommentService.ts
+++ 1-back-end-server/src/service/CommentService.ts
@@ -33,10 +33,12 @@ class CommentService {
       parentCommentId != null ? await this.getComment(parentCommentId) : null
     const rootId = parentComment?.parentCommentId
     const parentId = rootId ?? parentCommentId
-    const metionUserId = parentComment?.userId
+    const mentionUserId = parentComment?.userId
 
-    const shouldMentionUser = rootId != null && metionUserId != null
     const isSubcomment = parentId != null
+    // 등록할 댓글이 하위댓글 이면서, 자신이 아닌 남을 언급했을경우, mention 허용!
+    const shouldMentionUser =
+      isSubcomment && mentionUserId != null && mentionUserId !== userId
 
     const newComment = await db.comment.create({
       data: {
@@ -44,7 +46,7 @@ class CommentService {
         text,
         userId,
         parentCommentId: isSubcomment ? parentId : null,
-        mentionUserId: shouldMentionUser ? metionUserId : null,
+        mentionUserId: shouldMentionUser ? mentionUserId : null,
       },
       include: { user: INCLUDE_SIMPLE_USER },
     })
