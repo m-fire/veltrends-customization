2ccea4c74d9e16f7d7764a2de1f53038e6fb4d25
countless-exercise <truestarmello@gmail.com>
2022-11-28 05:26:49 +0900

    add figma & SVG icon(more_vert.svg)

---
+++ 2-front-end-web/app/components/generate/svg/MoreVert.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import { SVGProps } from 'react'
+
+const SvgMoreVert = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    {...props}
+  >
+    <path
+      d="M8 16a1.481 1.481 0 1 1 1.049-.433A1.429 1.429 0 0 1 8 16ZM8 9.482a1.482 1.482 0 1 1 1.049-.433A1.428 1.428 0 0 1 8 9.482ZM8 2.963a1.481 1.481 0 1 1 1.049-.433A1.428 1.428 0 0 1 8 2.963Z"
+      fill="currentColor"
+    />
+  </svg>
+)
+
+export default SvgMoreVert
