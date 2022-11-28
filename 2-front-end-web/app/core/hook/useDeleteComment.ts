226fef1ef6bf45a3a297411c7ce614a116ec82dc
countless-exercise <truestarmello@gmail.com>
2022-11-29 02:35:39 +0900

    feat 댓글 삭제 2: useDeleteComment 훅 - 아이탬ID 를 구해, 댓글삭제 후, UI Model 에 삭제결과 반영

--- 2-front-end-web/app/core/hook/useDeleteComment.ts
+++ 2-front-end-web/app/core/hook/useDeleteComment.ts
@@ -1,7 +1,26 @@
+import { useQueryClient } from '@tanstack/react-query'
 import { useCallback } from 'react'
+import { deleteComment } from '~/core/api/items$comments'
+import { useItemIdParams } from './useItemIdParams'
+import { getCommentListQueryKey } from '~/core/hook/query/useCommentListQuery'
 
 export function useDeleteComment() {
-  const deleteCommentFn = useCallback(async (commentId: number) => {}, [])
+  const queryClient = useQueryClient()
+  const itemId = useItemIdParams()
+
+  const deleteCommentFn = useCallback(
+    async (commentId: number) => {
+      if (!itemId) return
+
+      await deleteComment({
+        commentId,
+        itemId,
+      })
+
+      await queryClient.invalidateQueries(getCommentListQueryKey(itemId))
+    },
+    [itemId, queryClient],
+  )
 
   return deleteCommentFn
 }
