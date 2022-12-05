62fc1d66ab952b43e04ce0ee9152796978b8fc56
countless-exercise <truestarmello@gmail.com>
2022-12-05 19:45:33 +0900

    feat /app/routes/search.tsx: TabLayout, StyledHeader, SearchInput 조립

--- 2-front-end-web/app/routes/search.tsx
+++ 2-front-end-web/app/routes/search.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
 import TabLayout from '~/common/component/layout/TabLayout'
+import Header from '~/common/component/base/Header'
+import SearchInput from '~/core/component/search/SearchInput'
 
-type SearchProps = {}
+function Search() {
+  const [searchText, setSearchText] = useState('')
 
-function Search({}: SearchProps) {
-  return <TabLayout>Search route</TabLayout>
+  return (
+    <TabLayout
+      header={
+        <StyledHeader
+          title={
+            <SearchInput value={searchText} onChangeText={setSearchText} />
+          }
+        />
+      }
+    />
+  )
 }
 export default Search
 
 // Inner Components
+
+const StyledHeader = styled(Header)`
+  & > .title {
+    width: 100%;
+  }
+`
