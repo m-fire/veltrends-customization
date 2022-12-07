07dbff455716a239b025d13ca0c6ce777e4fe3ea
countless-exercise <truestarmello@gmail.com>
2022-12-07 23:52:26 +0900

    refactor ranking-bots -> items-worker 파일명 변경

--- 1-back-end-server/src/worker/items-worker.ts
+++ 1-back-end-server/src/worker/items-worker.ts
@@ -5,29 +5,32 @@ import ItemStatusService from '../service/ItemStatusService.js'
 const itemStatusService = ItemStatusService.getInstance()
 
 const itemsWorker = {
-  async updateRankingScores() {
+  async updateAllRankingScores() {
     // @todo: increase concurrency after migrating to postresql
-    const concurrentLimitWorker = pLimit(1)
+    const limitWorker = pLimit(1)
+    /* todo: postgresql로 마이그레이션한 후 동시성 증가 확인할것 */
 
     // const targetStatusList = await this.itemStatusService.getRankTargetList(0.001)
     const targetStatusList = await itemStatusService.getRankTargetStatusList()
-    console.log(`ranking-bots.ts> ItemRankingWorker.updateRankingScores()`, {
+    console.log(`items-worker.ts> itemsWorker.updateAllRankingScores()`, {
       'target List size': targetStatusList.length,
     })
 
-    const rankingWorkerList = targetStatusList.map((targetStatus) => {
-      return concurrentLimitWorker(() => {
+    const rankingScoreUpdaters = targetStatusList.map((targetStatus) => {
+      return limitWorker(async () => {
         const {
           itemId,
           likeCount,
           item: { createdAt },
         } = targetStatus
         const score = RankCalculator.rankingScore(likeCount, createdAt)
-        itemStatusService.updateScore({ itemId, score })
+        await itemStatusService.updateScore({ itemId, score })
       })
     })
 
-    await Promise.allSettled(rankingWorkerList)
-    console.log(`ranking-bots.ts> RankingWorkers all successfully!`)
+    await Promise.allSettled(rankingScoreUpdaters)
+    console.log(
+      `items-worker.ts> itemsWorker.updateAllRankingScores() successfully!`,
+    )
   },
 }
