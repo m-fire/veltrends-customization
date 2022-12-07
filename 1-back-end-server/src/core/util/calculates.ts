9319ccaf00547b2d95e7eb7b8cdeeac163ce6573
countless-exercise <truestarmello@gmail.com>
2022-12-07 20:17:00 +0900

    feat core/util/calculates: 좋아요수와 지난시간으로 score 계산 구현: ItemsCalculator.rankingScore(likeCount, past)

---
+++ 1-back-end-server/src/core/util/calculates.ts
@@ -0,0 +1,19 @@
+import { DateCalculator } from '../../common/util/calculates.js'
+
+/**
+ * 24시간이 지난 항목의 점수를 현저히 떨어뜨릴 목적의 값
+ */
+const gravity = 1.4 // 1.8
+
+export class ItemsCalculator {
+  /**
+   * <h3>좋아요 갯수와 지난시간을 토대로 계산된 score 반환</h3>
+   * <ul>
+   *     <li>ref: https://medium.com/jp-tech/how-are-popular-ranking-algorithms-such-as-reddit-and-hacker-news-working-724e639ed9f7</li>
+   * </ul>
+   */
+  static rankingScore(likeCount: number, past: Date) {
+    const pastHours = DateCalculator.hoursFromNow(past)
+    return likeCount / Math.pow(pastHours + 2, gravity)
+  }
+}
