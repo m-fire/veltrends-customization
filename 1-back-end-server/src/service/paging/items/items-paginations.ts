e904e7da1323b5e9ce2e6875d2d5e1e963c16a31
countless-exercise <truestarmello@gmail.com>
2022-12-10 06:32:57 +0900

    docs Todo: /service/paging/items/*.ts: 기존 class 기반 모듈을 TS Type & Interface 기반으로 변경

--- 1-back-end-server/src/service/paging/items/items-paginations.ts
+++ 1-back-end-server/src/service/paging/items/items-paginations.ts
@@ -3,6 +3,8 @@ import RecentPaging from './RecentPaging.js'
 import TrendingPaging from './TrendingPaging.js'
 import PastPaging from './PastPaging.js'
 
+/* Todo:  기존 class 기반 모듈을 TS Type & Interface 기반으로 변경 */
+
 const itemsModePagination = {
   getPagination<K extends ItemsPagingMode>(mode: K) {
     switch (mode) {
@@ -18,5 +20,3 @@ const itemsModePagination = {
   },
 }
 export default itemsModePagination
-
-// abstraction
