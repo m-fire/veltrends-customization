70f3810b8f269df7dde1a602bfb168a1b537c78b
countless-exercise <truestarmello@gmail.com>
2022-12-10 06:15:20 +0900

    refactor validates.ts : yyyy-mm-dd 형식 검증유틸 yyyymmdd(date) 추출

--- 1-back-end-server/src/common/util/validates.ts
+++ 1-back-end-server/src/common/util/validates.ts
@@ -13,4 +13,12 @@ export class Validator {
       return /^https?:\/\//.test(url)
     }
   }
+  static DateFormat = class DateFormatValidator {
+    /**
+     * yyyy-mm-dd 형태의 날짜형식 문자열을 검사합니다.
+     */
+    static yyyymmdd(date: string) {
+      return !/^\d{4}-\d{2}-\d{2}$/.test(date)
+    }
+  }
 }
