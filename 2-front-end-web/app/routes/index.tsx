57ffd949b4d6a33f0982cbed3d4c1a3a7c994b28
countless-exercise <truestarmello@gmail.com>
2022-12-12 03:50:37 +0900

    feat routes/index.tsx: 모드별로 items api 서버 요청인수 추가: mode, startDate, endDate

--- 2-front-end-web/app/routes/index.tsx
+++ 2-front-end-web/app/routes/index.tsx
@@ -2,23 +2,33 @@ import { useCallback, useEffect, useRef, useState } from 'react'
 import TabLayout from '~/common/component/layout/TabLayout'
 import { json, LoaderFunction } from '@remix-run/node'
 import { getItemList } from '~/core/api/items'
-import { useFetcher, useLoaderData } from '@remix-run/react'
-import { ItemListPagination } from '~/core/api/types'
+import { useFetcher, useLoaderData, useNavigate } from '@remix-run/react'
+import { ItemListPagination, ListMode } from '~/core/api/types'
 import LinkCardList from '~/core/component/home/LinkCardList'
 import { Requests } from '~/common/util/https'
 import { useInfiniteScroll } from '~/common/hook/useInfiniteScroll'
 
 function Index() {
   const data = useLoaderData<ItemListPagination>()
+  useEffect(() => {
+    console.log(`index.tsx> API 서버에서 받아온 페이지데이터`, { data })
+  }, [data])
+
   const [pages, setPages] = useState([data])
   const fetcher = useFetcher<ItemListPagination>()
-
-  // 새 페이지가 있으면, 기존목록에 새 페이지 추가
   useEffect(() => {
     const page = fetcher.data
+    // 새 페이지가 있으면, 기존목록에 새 페이지 추가
     if (!page || pages.includes(page)) return
     setPages(pages.concat(page))
   }, [fetcher.data, pages])
+
+  const [mode, setMode] = useState<ListMode>('trending')
+  const navigate = useNavigate()
+  useEffect(() => {
+    const nextUrl = mode === 'trending' ? '/' : `/?mode=${mode}`
+    navigate(nextUrl, { replace: true })
+  }, [mode, navigate])
 
   // 다음 목록페이지 요청
   const fetchNext = useCallback(() => {
@@ -51,10 +61,27 @@ export default Index
 // Remix handler
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const { cursor } = Requests.parseUrlParams<{ cursor?: string }>(request.url)
+  const { cursor, mode, startDate, endDate } = Requests.parseUrlParams<{
+    mode?: string
+    cursor?: string
+    startDate?: string
+    endDate?: string
+  }>(request.url)
+
+  const fallbackedMode = mode ?? 'trending'
   const parsedCursor = cursor != null ? parseInt(cursor, 10) : undefined
 
-  const itemList = await getItemList(parsedCursor)
+  console.log(fallbackedMode)
+  // @todo: throw error if invalid error
+
+  const itemList = await getItemList({
+    mode: fallbackedMode as any,
+    cursor: parsedCursor,
+
+    // todo: 임시 Past 문자, 실제기능 구현
+    startDate: '2022-12-12',
+    endDate: '2022-12-06',
+  })
   return json(itemList)
 }
 
