a84649cb44e049ca0cbf3890e095e28f935b736d
countless-exercise <truestarmello@gmail.com>
2022-12-17 07:46:22 +0900

    refactor ListModeSelector: 기존의 버튼기반을 Link 기반으로 변경

--- 2-front-end-web/app/core/component/home/ListModeSelector.tsx
+++ 2-front-end-web/app/core/component/home/ListModeSelector.tsx
@@ -7,62 +7,68 @@ import {
   useRef,
   useState,
 } from 'react'
+import { Link } from '@remix-run/react'
 import styled, { css } from 'styled-components'
 import { motion } from 'framer-motion'
 import produce from 'immer'
+import qs from 'qs'
 import { colors } from '~/common/style/colors'
 import { ListMode } from '~/core/api/types'
 import { Calendar, Clock, Fire } from '~/core/component/generate/svg'
 import { flexStyles, fontStyles } from '~/common/style/styled'
+import { DateStringRange } from '~/common/util/converters'
 
-type ListModeSelectorProps = {
-  mode: ListMode
-  onSelectMode(mode: ListMode): void
+type ModeSelectorProps = {
+  currentMode: ListMode
+  dateRange: DateStringRange
 }
 
-function ListModeSelector({ mode, onSelectMode }: ListModeSelectorProps) {
-  const modeStateList = useMemo(
-    () =>
-      [
-        {
-          mode: 'trending',
-          name: '트렌딩',
-          icon: <Fire />,
-        },
-        {
-          mode: 'recent',
-          name: '최신',
-          icon: <Clock />,
-        },
-        {
-          mode: 'past',
-          name: '과거',
-          icon: <Calendar />,
-        },
-      ] as const,
-    [],
-  )
-  const currentIndex = useMemo(
-    () => modeStateList.findIndex((s) => s.mode === mode),
-    [modeStateList, mode],
-  )
-
-  const [itemStates, setItemStates] = useState<SelectorItemState[]>([
-    { size: { width: 0 }, position: { left: 0 } },
-    { size: { width: 0 }, position: { left: 0 } },
-    { size: { width: 0 }, position: { left: 0 } },
+function ListModeSelector({ currentMode, dateRange }: ModeSelectorProps) {
+  const [itemStates, setItemStates] = useState<ModeItemProps['state'][]>([
+    {
+      mode: 'trending',
+      name: '트렌딩',
+      icon: <Fire />,
+      size: { width: 0 },
+      position: { left: 0 },
+      linkTo: linkByMode('trending'),
+    },
+    {
+      mode: 'recent',
+      name: '최신',
+      icon: <Clock />,
+      size: { width: 0 },
+      position: { left: 0 },
+      linkTo: linkByMode('recent'),
+    },
+    {
+      mode: 'past',
+      name: '과거',
+      icon: <Calendar />,
+      size: { width: 0 },
+      position: { left: 0 },
+      linkTo: linkByMode('past', dateRange),
+    },
   ])
-  const indicatorLeft = useMemo(
-    () => itemStates[currentIndex].position.left,
-    [currentIndex, itemStates],
-  )
-  const indicatorWidth = itemStates[currentIndex].size.width
 
-  const setItemSizeOfIndex: SelectorItemProps['onUpdateState'] = useCallback(
-    (index: number, stateOfIndex) => {
-      setItemStates((prevState) =>
-        produce(prevState, (draft) => {
-          draft[index] = stateOfIndex
+  const currentIndex = useMemoCurrentItemIndex()
+  const { indicatorLeft, indicatorWidth } = useIndicatorState(currentIndex)
+
+  useEffectToUpdatePastSelectorLinkDate(currentIndex)
+
+  const onUpdateItemStates: ModeItemProps['onUpdateItemState'] = useCallback(
+    (itemIndex, itemState, itemEl) => {
+      setItemStates((prev) =>
+        produce(prev, (next) => {
+          const indicatorMarginWidth = 5
+          next[itemIndex].size = {
+            ...itemState.size,
+            width: itemEl.offsetWidth + indicatorMarginWidth,
+          }
+          next[itemIndex].position = {
+            ...itemState.position,
+            left: itemEl.offsetLeft - indicatorMarginWidth / 3,
+          }
         }),
       )
     },
@@ -72,17 +78,17 @@ function ListModeSelector({ mode, onSelectMode }: ListModeSelectorProps) {
   return (
     <Block>
       <SelectorList>
-        {modeStateList.map((state, index) => (
-          <SelectorItem
+        {itemStates.map((s, index) => (
+          <ModeItem
+            key={s.name}
             index={index}
-            key={state.name}
-            currentMode={mode}
-            modeState={state}
-            onSelectMode={onSelectMode}
-            onUpdateState={setItemSizeOfIndex}
+            currentMode={currentMode}
+            state={s}
+            onUpdateItemState={onUpdateItemStates}
           />
         ))}
-        {indicatorWidth === 0 ? null : (
+
+        {indicatorWidth > 0 ? (
           <Indicator
             layout
             style={{
@@ -90,10 +96,49 @@ function ListModeSelector({ mode, onSelectMode }: ListModeSelectorProps) {
               left: indicatorLeft,
             }}
           />
-        )}
+        ) : null}
       </SelectorList>
     </Block>
   )
+
+  /* refactor */
+
+  function useEffectToUpdatePastSelectorLinkDate(index: number) {
+    useEffect(() => {
+      if (currentMode == 'past') {
+        setItemStates((prev) =>
+          produce(prev, (next) => {
+            next[index].linkTo = linkByMode(currentMode, dateRange)
+          }),
+        )
+      }
+    }, [dateRange])
+  }
+
+  function useMemoCurrentItemIndex() {
+    return useMemo(
+      () => itemStates.findIndex((s) => s.mode === currentMode),
+      [itemStates, currentMode],
+    )
+  }
+
+  function linkByMode(mode: ListMode, range?: DateStringRange) {
+    const paramsMap = mode == 'past' ? { mode, ...range } : { mode }
+    return '/'.concat(qs.stringify(paramsMap, { addQueryPrefix: true }))
+  }
+
+  function useIndicatorState(itemIndex: number) {
+    const indicatorLeft = useMemo(
+      () => itemStates[itemIndex].position.left,
+      [itemIndex, itemStates],
+    )
+    const indicatorWidth = itemStates[itemIndex].size.width
+
+    return {
+      indicatorLeft,
+      indicatorWidth,
+    }
+  }
 }
 export default ListModeSelector
 
@@ -103,13 +148,14 @@ const Block = styled.div`
   position: sticky;
   ${flexStyles({ justifyContent: 'center' })};
   top: 0;
+
   // todo: 가로 사이즈가 꽉 차도록 수정
   padding-top: 8px;
   padding-bottom: 16px;
   margin-bottom: 16px;
 
-  backdrop-filter: grayscale(80%) blur(8px);
-  -webkit-backdrop-filter: grayscale(80%) blur(8px);
+  backdrop-filter: grayscale(80%) blur(4px);
+  -webkit-backdrop-filter: grayscale(80%) blur(4px);
   z-index: 1;
 `
 
@@ -128,81 +174,77 @@ const Indicator = styled(motion.div)`
   border-radius: 99px;
 `
 
-// Inner Function Component
+// Inner function component
 
-type SelectorItemProps = {
+type ModeItemProps = {
   index: number
   currentMode: ListMode
-  modeState: {
-    mode: ListMode
-    name: string
-    icon: ReactNode
-  }
-  onSelectMode: ListModeSelectorProps['onSelectMode']
-  onUpdateState(index: number, state: SelectorItemState): void
+  state: ModeItemState
+  onUpdateItemState: (
+    itemIndex: number,
+    itemState: ModeItemState,
+    itemEl: HTMLElement,
+  ) => void
 }
-
-type SelectorItemState = {
+type ModeItemState = {
+  mode: ListMode
+  name: string
+  icon: ReactNode
   size: { width: number }
   position: { left: number }
+  linkTo: string
 }
 
-/**
- * Inner function component
- */
-function SelectorItem({
+export function ModeItem({
   index,
   currentMode,
-  modeState: { mode, name, icon },
-  onSelectMode,
-  onUpdateState,
-}: SelectorItemProps) {
-  const modeItemRef = useRef<HTMLDivElement>(null)
+  state,
+  onUpdateItemState,
+}: ModeItemProps) {
+  const { mode: itemMode, linkTo, name, icon } = state
+  /* Link 이동 후 re-render 될때 Item state 업데이트 로직 실행 */
 
-  /* ref: https://medium.com/@jnso5072/react-useeffect-%EC%99%80-uselayouteffect-%EC%9D%98-%EC%B0%A8%EC%9D%B4%EB%8A%94-%EB%AC%B4%EC%97%87%EC%9D%BC%EA%B9%8C-e1a13adf1cd5
-   * useEffect 대신 useLayoutEffect 를 사용해야 하는 상황:
-   * useEffect 사용시 발생하는 first-rendering -> re-rendering 사이의 깜빡거림 때문에
-   * 첫 painting 시 조건에 따라 state 가 다르게 렌더링 해야 한다면 useLayoutEffect 사용 */
-  // useLayoutEffect(() => {
   useEffect(() => {
-    const itemEl = modeItemRef.current
-    if (!itemEl) return
+    // useLayoutEffect(() => {
+    const linkEl = modeLinkRef.current
+    if (!linkEl) return
+    onUpdateItemState(index, state, linkEl)
+  }, [index, onUpdateItemState])
 
-    const marginWidth = 8
-    onUpdateState(index, {
-      size: { width: itemEl.offsetWidth + marginWidth },
-      position: { left: itemEl.offsetLeft - marginWidth / 2 },
-    })
-  }, [onUpdateState, index])
+  const modeLinkRef = useRef<HTMLAnchorElement>(null)
 
   return (
-    <ModeBlock
-      ref={modeItemRef}
-      isActive={mode === currentMode}
-      onClick={() => onSelectMode(mode)}
-    >
+    <StyledLink to={linkTo} ref={modeLinkRef} active={itemMode === currentMode}>
       {icon} {name}
-    </ModeBlock>
+    </StyledLink>
   )
 }
 
-const ModeBlock = styled.div<{ isActive?: boolean }>`
+const StyledLink = styled(Link)<{ active?: boolean }>`
   ${flexStyles({ alignItems: 'flex-end' })};
   ${fontStyles({
     size: '14px',
     color: colors.grey4,
-    weight: 700,
-    // lineHeight: 1.5,
+    weight: 700, // lineHeight: 1.5,
   })};
-  ${(props) =>
-    props.isActive &&
-    css`
-      color: ${colors.primary1};
-    `}
+  text-decoration: none;
+  ///* white border 처리 */
+  -webkit-filter: drop-shadow(0px 0px 0.5px white)
+    drop-shadow(0px 0px 0.5px white) drop-shadow(0px 0px 0.5px white)
+    drop-shadow(0px 0px 0.5px white);
+  filter: drop-shadow(0px 0px 0.5px white) drop-shadow(0px 0px 0.5px white)
+    drop-shadow(0px 0px 0.5px white) drop-shadow(0px 0px 0.5px white);
 
+  ${({ active }) =>
+    active
+      ? css`
+          color: ${colors.primary1};
+          pointer-events: none;
+        `
+      : css``}
   svg {
     //width: 20px;
-    height: 20px;
+    height: 18px;
     margin-right: 4px;
   }
 `
