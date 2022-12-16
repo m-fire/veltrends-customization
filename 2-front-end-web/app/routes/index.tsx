9e56f3726bf60d38331cf8146e417040d9999ace
countless-exercise <truestarmello@gmail.com>
2022-12-17 07:44:19 +0900

    feat & refactor routes/index.tsx: ListModeSelector 마운트 및 past URL쿼리 받기, 및 복잡한로직 리팩토링

--- 2-front-end-web/app/routes/index.tsx
+++ 2-front-end-web/app/routes/index.tsx
@@ -1,86 +1,162 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
-import TabLayout from '~/common/component/layout/TabLayout'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useLoaderData, useSearchParams } from '@remix-run/react'
 import { json, LoaderFunction } from '@remix-run/node'
-import { getItemList } from '~/core/api/items'
-import { useLoaderData, useNavigate, useSearchParams } from '@remix-run/react'
-import { ItemListPagination, ListMode } from '~/core/api/types'
-import LinkCardList from '~/core/component/home/LinkCardList'
-import { Requests } from '~/common/util/https'
-import { useInfiniteScroll } from '~/common/hook/useInfiniteScroll'
-import ListModeSelector from '~/core/component/home/ListModeSelector'
 import { useInfiniteQuery } from '@tanstack/react-query'
+import { Requests } from '~/common/util/https'
+import { getItemList } from '~/core/api/items'
+import { ItemListPagination, ListMode } from '~/core/api/types'
+import { useInfiniteScroll } from '~/common/hook/useInfiniteScroll'
+import TabLayout from '~/common/component/layout/TabLayout'
+import LinkCardList from '~/core/component/home/LinkCardList'
+import ListModeSelector from '~/core/component/home/ListModeSelector'
+import DateRangeSelector from '~/core/component/home/DateRangeSelector'
+import {
+  DateStringRange,
+  WeekRangeConverters as wrc,
+} from '~/common/util/converters'
 
 function Index() {
-  // URL Parameter 에서 초기값 가져와서 state 바인딩
-  const [searchParams] = useSearchParams()
-  const modeParam = (searchParams.get('mode') as any) ?? 'trending'
-
-  // API 서버로부터 데이터로딩 with 1-remix-fetch & 2-react-query
-  const initialData = useLoaderData<ItemListPagination>()
-  const [mode, setMode] = useState<ListMode>(modeParam)
-  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(
-    ['items', mode],
-    ({ pageParam }) => getItemList({ mode, cursor: pageParam }),
-    {
-      initialData: {
-        pageParams: [undefined],
-        pages: [initialData],
-      },
-      getNextPageParam: (lastPage) => {
-        if (!lastPage.pageInfo.hasNextPage) return null
-        return lastPage.pageInfo.lastCursor // pageParam 로 사용
-      },
-    },
+  const defaultDateRange = useCurrentDateRange()
+  const {
+    mode,
+    dateRange: { start, end },
+  } = getCurrentURLParams('trending', defaultDateRange)
+  const [dateRange, setDateRange] = useState<DateStringRange>({ start, end })
+  const { data, hasNextPage, fetchNextPage } = useItemsInfiniteQuery(
+    ['items', mode, mode === 'past' ? { start } : undefined].filter(
+      (key) => key != null,
+    ),
+    { mode, dateRange },
   )
 
-  // 모드가 변경될 때마다, 모드별 요청 파라미터 변경 후 이동
-  const navigate = useNavigate()
   useEffect(() => {
-    const nextUrl = mode === 'trending' ? '/' : `/?mode=${mode}`
-    navigate(nextUrl, { replace: true })
-  }, [mode, navigate])
+    if (mode === 'past') {
+      setDateRange({ start, end })
+    }
+  }, [mode, start, end])
 
-  // 감지영역에 screen 이 도달한 경우, react-query 를 통해 다음목록 붙이기
-  const fetchNext = useCallback(async () => {
-    if (!hasNextPage) return
-    // Fixed: react-query 의 fetchNextPage() 를 그대로 무한스크롤로딩에 붙이면,
-    // 기존 목록에 새로 불러온 데이터를 덧붙게 된다. 이를 막기위해
-    // 한번 로딩이 되었다면, 기존 데이터 외에 받은 데이터만 붙이도록 수정.
-    await fetchNextPage()
-  }, [fetchNextPage, hasNextPage])
-  const intersectionRef = useRef<HTMLDivElement>(null)
-  useInfiniteScroll(intersectionRef, fetchNext)
+  const intersectionRef = useInfinityScrollIntersectionRef<HTMLDivElement>({
+    hasNextPage,
+    fetchNextPage,
+  })
 
-  const items = data?.pages.flatMap((page) => page.list)
+  const itemList = data?.pages.flatMap((page) => page.list)
   return (
     <TabLayout>
-      <ListModeSelector mode={mode} onSelectMode={setMode} />
-      {items ? <LinkCardList items={items} /> : null}
+      <ListModeSelector currentMode={mode} dateRange={dateRange} />
+
+      {mode === 'past' ? (
+        <DateRangeSelector
+          baseLinkTo={`/?mode=${mode}`}
+          dateRange={dateRange}
+        />
+      ) : null}
+
+      {itemList ? <LinkCardList items={itemList} /> : null}
+
+      {/* Infinity Scroll Intersection Area */}
       <div ref={intersectionRef} />
     </TabLayout>
   )
+
+  /* refactoring */
+
+  function useInfinityScrollIntersectionRef<E extends HTMLElement>({
+    hasNextPage,
+    fetchNextPage,
+  }: Partial<ReturnType<typeof useInfiniteQuery>>) {
+    /* 무한 스크롤 로딩 Ref 및 기능정의 */
+    const ref = useRef<E>(null)
+
+    const fetchNext = useCallback(() => {
+      if (hasNextPage === false) return
+      fetchNextPage?.()
+    }, [hasNextPage, fetchNextPage])
+
+    useInfiniteScroll(ref, fetchNext)
+    return ref
+  }
+  type UseItemsInfiniteQueryParams = {
+    mode: ListMode
+    dateRange: DateStringRange
+  }
+
+  function useItemsInfiniteQuery(
+    queryKey: Parameters<typeof useInfiniteQuery>[0],
+    { mode, dateRange }: UseItemsInfiniteQueryParams,
+  ) {
+    const initialData = useLoaderData<ItemListPagination>()
+    const queryResult = useInfiniteQuery(
+      queryKey,
+      async ({ pageParam: cursor }) => {
+        const { start, end } = dateRange
+        const itemsPage = await getItemList({
+          mode,
+          cursor,
+          ...(mode === 'past' ? { startDate: start, endDate: end } : undefined),
+        })
+        return itemsPage
+      },
+      {
+        initialData: {
+          pageParams: [undefined],
+          pages: [initialData], // from Remix loader
+        },
+        getNextPageParam: (lastPage) => {
+          if (!lastPage.pageInfo.hasNextPage) return null
+          return lastPage.pageInfo.lastCursor
+        },
+      },
+    )
+    return queryResult
+  }
+
+  function useCurrentDateRange() {
+    return useMemo(() => wrc.toRangeString(new Date()), [])
+  }
+
+  function getCurrentURLParams(
+    defaultMode: ListMode,
+    defaultDateRange: DateStringRange,
+  ) {
+    const [searchParams] = useSearchParams()
+    return {
+      mode: (searchParams.get('mode') as ListMode) ?? defaultMode,
+      dateRange: {
+        start: searchParams.get('start') ?? defaultDateRange.start,
+        end: searchParams.get('end') ?? defaultDateRange.end,
+      },
+    }
+  }
 }
 export default Index
 
 // Remix handler
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const { mode } = Requests.parseUrlParams<{ mode?: string }>(request.url)
+  const { mode, start, end } = Requests.parseUrlParams<HomeRequestURLParams>(
+    request.url,
+  )
 
-  const fallbackedMode = mode ?? 'trending'
+  const fallbackMode = mode ?? 'trending'
+  const defaultRange = wrc.toRangeString(new Date())
+  const dateRange = mode === 'past' ? defaultRange : undefined
 
-  console.log(fallbackedMode)
-  // @todo: throw error if invalid error
-
-  const itemList = await getItemList({
-    mode: fallbackedMode as any,
-    cursor: parsedCursor,
-
-    // todo: 임시 Past 문자, 실제기능 구현
-    startDate: '2022-12-12',
-    endDate: '2022-12-06',
+  const itemListPage = await getItemList({
+    mode: fallbackMode as any,
+    startDate: start ?? dateRange?.start,
+    endDate: end ?? dateRange?.end,
   })
-  return json(itemList) // to useLoaderData() hook
+
+  return json(itemListPage) // to useLoaderData() hook
 }
 
 // Inner Components
+
+// types
+
+type HomeRequestURLParams = {
+  mode: string | null
+  start: string | null
+  end: string | null
+}
