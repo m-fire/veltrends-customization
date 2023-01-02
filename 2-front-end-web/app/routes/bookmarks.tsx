8beae9481b7aaf4766226341a981640a6f455c5f
countless-exercise <truestarmello@gmail.com>
2023-01-03 02:25:00 +0900

    feat routes/bookmarks: 북마크 라우터 구현 1: 인증사용자 검증 및 북마크목록 가져오기

--- 2-front-end-web/app/routes/bookmarks.tsx
+++ 2-front-end-web/app/routes/bookmarks.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import TabLayout from '~/common/component/layout/TabLayout'
+import { json, LoaderFunction, redirect } from '@remix-run/node'
+import { Authenticator } from '~/core/api/auth'
+import { getBookmarkItemList } from '~/core/api/bookmarks'
+import { useLoaderData } from '@remix-run/react'
 
-type BookmarksProps = {}
+function Bookmarks() {
+  const initialData = useLoaderData<BookmarkItemList>()
 
-function Bookmarks({}: BookmarksProps) {
   return <TabLayout>Bookmarks route</TabLayout>
 }
 export default Bookmarks
 
+export const loader: LoaderFunction = async ({ request }) => {
+  const isAuthenticated = await Authenticator.checkAuthenticated(request)
+  if (!isAuthenticated) return redirect(`/auth/login?next=/bookmarks`)
+
+  const bookmarkList = await getBookmarkItemList()
+  return json(bookmarkList)
+}
+
 // Inner Components
+
+// types
+
+type BookmarkItemList = Awaited<ReturnType<typeof getBookmarkItemList>>
