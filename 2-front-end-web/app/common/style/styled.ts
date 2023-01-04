a949f7f6e1f61dc0b74f037d39e3524883d729b8
countless-exercise <truestarmello@gmail.com>
2023-01-04 15:55:16 +0900

    feat common/styles.ts: gridContainer, gridItem 스타일링 함수 추가

--- 2-front-end-web/app/common/style/styled.ts
+++ 2-front-end-web/app/common/style/styled.ts
@@ -74,6 +74,114 @@ type FlexItemParams = {
   alignSelf?: CSSProperties['alignSelf']
 }
 
+export function gridContiner({
+  display = 'grid',
+  templateRows,
+  templateColumns,
+  autoRows,
+  autoColumns,
+  autoFlow,
+  alignContent,
+  alignItems,
+  justifyContent,
+  justifyItems,
+}: GridContinerParams = {}) {
+  return css`
+    display: ${display};
+
+    ${templateRows &&
+    css`
+      grid-template-rows: ${templateRows};
+    `}
+    ${templateColumns &&
+    css`
+      grid-template-columns: ${templateColumns};
+    `}
+    ${autoRows &&
+    css`
+      grid-auto-rows: ${autoRows};
+    `}
+    ${autoColumns &&
+    css`
+      grid-auto-columns: ${autoColumns};
+    `}
+    ${autoFlow &&
+    css`
+      grid-auto-flow: ${autoFlow};
+    `}
+    ${alignContent &&
+    css`
+      align-content: ${alignContent};
+    `}
+    ${alignItems &&
+    css`
+      align-items: ${alignItems};
+    `}
+    ${justifyContent &&
+    css`
+      justify-content: ${justifyContent};
+    `}
+    ${justifyItems &&
+    css`
+      justify-items: ${justifyItems};
+    `}
+  `
+}
+
+type GridContinerParams = {
+  display?: 'grid' | 'inline-grid'
+
+  // 명시적 가로 길이(number + units/ repeat(number + units)
+  templateRows?: CSSProperties['gridTemplateRows']
+  templateColumns?: CSSProperties['gridTemplateColumns']
+  // 암시적 가로 Item 갯수(number + units/ repeat(number + units)
+  autoRows?: CSSProperties['gridAutoRows']
+  autoColumns?: CSSProperties['gridAutoColumns']
+  // 자동배치 알고리즘 종류설정
+  autoFlow?: CSSProperties['gridAutoFlow']
+
+  // 행 시작 위치
+  rowStart?: CSSProperties['gridRowStart']
+  // 행 끝 위치
+  rowEnd?: CSSProperties['gridRowEnd']
+  // 열 시작 위치
+  columnStart?: CSSProperties['gridColumnStart']
+  // 열 끝 위치
+  columnEnd?: CSSProperties['gridColumnEnd']
+
+  // Grid 의 수직 열,축 정렬
+  alignContent?: CSSProperties['alignContent']
+  // 모든 Grid Item 수직 열,축 정렬
+  alignItems?: CSSProperties['alignItems']
+  // Grid 의 수평 열,축 정렬
+  justifyContent?: CSSProperties['justifyContent']
+  // 모든 Grid Item 수평 열,축 정렬
+  justifyItems?: CSSProperties['justifyItems']
+}
+
+export function gridItem({
+  alignSelf,
+  justifySelf,
+}: GridItemStylesParams = {}) {
+  return css`
+    ${alignSelf &&
+    css`
+      align-self: ${alignSelf};
+    `}
+    ${justifySelf &&
+    css`
+      justify-self: ${justifySelf};
+    `}
+  `
+}
+
+type GridItemStylesParams = {
+  // Grid 의 수직 열,축 정렬
+  alignSelf?: CSSProperties['alignSelf']
+  // 모든 Grid Item 수직 열,축 정렬
+  justifySelf?: CSSProperties['justifySelf']
+}
+
 export function fontStyles({
   size,
   weight,
