7574e3059386f3835132f5ea93c14091b92d3bb0
countless-exercise <truestarmello@gmail.com>
2023-01-15 05:58:44 +0900

    add assets: person.svg 추가후 -> Person.tsx 변환

---
+++ 2-front-end-web/app/core/component/generate/svg/Person.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import { SVGProps } from 'react'
+
+const SvgPerson = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    {...props}
+  >
+    <path
+      d="M8 8c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4Zm0 2c-2.67 0-8 1.34-8 4v1c0 .55.45 1 1 1h14c.55 0 1-.45 1-1v-1c0-2.66-5.33-4-8-4Z"
+      fill="currentColor"
+    />
+  </svg>
+)
+
+export default SvgPerson
