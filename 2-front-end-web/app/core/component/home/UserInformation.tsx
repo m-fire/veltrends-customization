c5f9e4c1e67e8b8e4566469124b10ce92c64941e
countless-exercise <truestarmello@gmail.com>
2023-01-16 11:40:24 +0900

    feat: UserInformation: 컨택스트메뉴 배치, 매뉴항목배치 및 클릭&터치 이벤트 핸들링

--- 2-front-end-web/app/core/component/home/UserInformation.tsx
+++ 2-front-end-web/app/core/component/home/UserInformation.tsx
@@ -1,9 +1,9 @@
-import React, { ReactNode } from 'react'
-import VariantLinkButton from '~/core/component/VariantLinkButton'
+import React, { ReactNode, useState } from 'react'
+import VariantLinkOrButton from '~/core/component/VariantLinkOrButton'
 import { Person } from '~/core/component/generate/svg'
 import styled from 'styled-components'
-import { decorateStyles } from '~/core/style/decorate-styles'
 import { Flex } from '~/common/style/css-builder'
+import ContextMenu from '~/common/component/template/ContextMenu'
 
 type UserInformationProps = {
   username: string
@@ -11,14 +11,38 @@ type UserInformationProps = {
 }
 
 function UserInformation({ username }: UserInformationProps) {
+  const [visible, setVisible] = useState(false)
+
+  const onToggleMenu = () => setVisible(!visible)
+
   return (
     <Responsive>
-      <VariantLinkButton variant="textonly">
-        <Icon>
-          {username}
+      <VariantLinkOrButton
+        variant="textonly"
+        size="small"
+        onClick={() => onToggleMenu()}
+        disabled={visible}
+      >
+        <AuthUser>
           <StyledPerson />
-        </Icon>
-      </VariantLinkButton>
+          {username}
+        </AuthUser>
+      </VariantLinkOrButton>
+
+      <StyledContextMenu onClose={onToggleMenu} visible={visible}>
+        <VariantLinkOrButton to="/write" variant="textonly" size="medium">
+          새글등록
+        </VariantLinkOrButton>
+        <VariantLinkOrButton to="/account" variant="textonly" size="medium">
+          내 계정
+        </VariantLinkOrButton>
+        <VariantLinkOrButton to="/bookmarks" variant="textonly" size="medium">
+          북마크
+        </VariantLinkOrButton>
+        <VariantLinkOrButton to="/" variant="textonly" size="medium">
+          로그아웃
+        </VariantLinkOrButton>
+      </StyledContextMenu>
     </Responsive>
   )
 }
@@ -31,8 +55,8 @@ const Responsive = styled.div`
   position: relative;
 `
 
-const Icon = styled.span`
-  ${Flex.container().alignItems('center').create()};
+const AuthUser = styled.span`
+  ${Flex.container().alignItems('center').justifyContent('flex-end').create()};
   svg {
     margin-right: 8px;
     width: 20px;
@@ -42,4 +66,8 @@ const Icon = styled.span`
 
 const StyledPerson = styled(Person)`
   width: 20px;
+`
+
+const StyledContextMenu = styled(ContextMenu)`
+  width: 130px;
 `
