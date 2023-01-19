f7b20fe417c645bc5f9587933fbe8c685b3b24cc
countless-exercise <truestarmello@gmail.com>
2023-01-20 06:04:43 +0900

    feat routes/api/me/: 라우터에 스키마 적용

--- 1-back-end-server/src/routes/api/me/index.ts
+++ 1-back-end-server/src/routes/api/me/index.ts
@@ -1,14 +1,28 @@
 import { FastifyPluginAsync } from 'fastify'
-import { ME_GET_SCHEMA } from './schema.js'
+import { ME_SCHEMA } from './schema.js'
 import { endpointAuthPlugin } from '../../../common/config/fastify/plugin/auth-plugins.js'
+import { MeRequestMap } from './types.js'
 
 const meRoute: FastifyPluginAsync = async (fastify) => {
   /* 특정 라우트에 인증정보검증 플러그인을 적용함. */
   fastify.register(endpointAuthPlugin) // URI`/api/me` 안에서만 보안검증
 
-  fastify.get('/', { schema: ME_GET_SCHEMA }, async (request) => {
-    return request.user
-  })
+  fastify.get<MeRequestMap['GET_ACCOUNT']>(
+    '/',
+    { schema: ME_SCHEMA.GET_ACCOUNT },
+    async (request) => {
+      return request.user?.id!
+    },
+  )
+
+  fastify.post<MeRequestMap['CHANGE_PASSWORD']>(
+    '/change-password',
+    { schema: ME_SCHEMA.CHANGE_PASSWORD },
+    async (request, reply) => {
+      const { oldPassword, newPassword } = request.body
+      reply.statusCode = 202
+    },
+  )
 }
 
 export default meRoute
