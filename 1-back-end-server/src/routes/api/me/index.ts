127db9fe351e7fee668f017061e4c6150df1d7d9
countless-exercise <truestarmello@gmail.com>
2023-01-20 10:22:54 +0900

    Fix src\routes\api\me\index.ts: GET_ACCOUNT 응답값 수정

--- 1-back-end-server/src/routes/api/me/index.ts
+++ 1-back-end-server/src/routes/api/me/index.ts
@@ -7,13 +7,14 @@ import { clearCookie } from '../../../common/config/jwt/cookies.js'
 
 const meRoute: FastifyPluginAsync = async (fastify) => {
   /* 특정 라우트에 인증정보검증 플러그인을 적용함. */
+  // 이것으로 인해, user 는 항상 인증상태를 유지함(항상 데이터가 존재)
   fastify.register(endpointAuthPlugin) // URI`/api/me` 안에서만 보안검증
 
   fastify.get<MeRequestMap['GET_ACCOUNT']>(
     '/',
     { schema: ME_SCHEMA.GET_ACCOUNT },
     async (request) => {
-      return request.user?.id!
+      return request.user
     },
   )
 
