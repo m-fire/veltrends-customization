e38da2fdf6dcea8fc723cf4888b23ee51953426f
countless-exercise <truestarmello@gmail.com>
2023-01-20 10:20:39 +0900

    refactor search 스키마 타입정리

--- 1-back-end-server/src/routes/api/search/schema.ts
+++ 1-back-end-server/src/routes/api/search/schema.ts
@@ -5,38 +5,34 @@ import {
   createPaginationSchema,
 } from '../../../common/config/typebox/schema-util.js'
 
-const REQ_SEARCH_ITEM_QUERYSTRING_SCHEMA = Type.Object({
-  q: Type.String(),
-  offset: Type.Optional(Type.Integer()),
-  limit: Type.Optional(Type.Integer()),
-})
-
-const RES_SEARCHED_ITEM_SCHEMA = Type.Object({
-  id: Type.Number(),
-  title: Type.String(),
-  body: Type.String(),
-  author: Nullable(Type.String()),
-  link: Type.String(),
-  likeCount: Type.Number(),
-  publisher: Type.Object({
-    name: Type.String(),
-    favicon: Type.String(),
-    domain: Type.String(),
-  }),
-  highlight: Type.Object({
-    title: Type.String(),
-    body: Type.String(),
-  }),
-})
-
-/* Fastify Schema */
-
 export const SEARCH_SCHEMA = createFastifySchemaMap({
   SEARCH: {
     tags: ['search'],
-    querystring: REQ_SEARCH_ITEM_QUERYSTRING_SCHEMA,
+    querystring: Type.Object({
+      q: Type.String(),
+      offset: Type.Optional(Type.Integer()),
+      limit: Type.Optional(Type.Integer()),
+    }),
     response: {
-      200: createPaginationSchema(RES_SEARCHED_ITEM_SCHEMA),
+      200: createPaginationSchema(
+        Type.Object({
+          id: Type.Number(),
+          title: Type.String(),
+          body: Type.String(),
+          author: Nullable(Type.String()),
+          link: Type.String(),
+          likeCount: Type.Number(),
+          publisher: Type.Object({
+            name: Type.String(),
+            favicon: Type.String(),
+            domain: Type.String(),
+          }),
+          highlight: Type.Object({
+            title: Type.String(),
+            body: Type.String(),
+          }),
+        }),
+      ),
     },
   },
 })
