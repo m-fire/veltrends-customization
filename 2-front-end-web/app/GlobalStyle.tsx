bd73e1d3ba5e4d9d99cc83364a896ff8dafa6e5b
countless-exercise <truestarmello@gmail.com>
2023-01-24 12:38:20 +0900

    Fix GlobalStyle: 폰트로딩 CSS코드 분리 및 root 에서 전역 style.css 불러오기

--- 2-front-end-web/app/GlobalStyle.tsx
+++ 2-front-end-web/app/GlobalStyle.tsx
@@ -12,8 +12,6 @@ const GlobalStyle = createGlobalStyle`
 		box-sizing: inherit;
 	}
 	body {
-		/* Pretendard 폰트 가져오기 & font-family 설정 */
-		@import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css");
 		font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
 		margin: 0;
 	}
--- 2-front-end-web/app/root.tsx
+++ 2-front-end-web/app/root.tsx
@@ -1,3 +1,4 @@
+import './style.css'
 import React, { Suspense, useEffect, useState } from 'react'
 import type { LoaderFunction, MetaFunction } from '@remix-run/node'
 import {
