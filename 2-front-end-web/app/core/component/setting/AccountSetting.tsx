cbbe291fd4ecfc8d6c5d22a55bcc9ae9ea4ed7a2
countless-exercise <truestarmello@gmail.com>
2023-01-25 17:49:23 +0900

    feat AccountSetting: 계정정보(비번) 변경폼 구성, 스타일링 및 이벤트핸들러 매핑

--- 2-front-end-web/app/core/component/setting/AccountSetting.tsx
+++ 2-front-end-web/app/core/component/setting/AccountSetting.tsx
@@ -1,51 +1,128 @@
-import React from 'react'
-import { useUserState } from '~/common/store/user'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { useMutation } from '@tanstack/react-query'
 import { globalColors } from '~/common/style/global-colors'
+import { useUserState } from '~/common/store/user'
 import Input from '~/common/component/atom/Input'
-import VariantLinkOrButton from '~/core/component/VariantLinkOrButton'
 import { Flex, Font } from '~/common/style/css-builder'
-import { appColors } from '~/core/style/app-colors'
+import { Media } from '~/common/style/media-query'
+import VariantLinkOrButton from '~/core/component/VariantLinkOrButton'
+import useOpenDialog from '~/common/hook/useOpenDialog'
+import { UserInformation } from '~/core/api/me'
+import AppError from '~/common/error/AppError'
+
+const initialFormState = {
+  oldPassword: '',
+  newPassword: '',
+}
 
 type AccountSettingProps = {}
 
 function AccountSetting({ ...rest }: AccountSettingProps) {
   const { user } = useUserState()
-  if (!user) return null
+  const [form, setForm] = useState(initialFormState)
+  const openDialog = useOpenDialog()
 
+  const reset = () => setForm(initialFormState)
+
+  const { mutate: mutateChangePassword } = useMutation(
+    UserInformation.changePassword,
+    {
+      onSuccess: () => {
+        /* open Dialog */
+        reset()
+      },
+      onError: (e) => {
+        const error = AppError.of(e)
+        switch (error.name) {
+          case 'BadRequest':
+            return /* open Dialog */
+          case 'Forbidden':
+            return /* open Dialog */
+          default:
+            throw error
+        }
+      },
+    },
+  )
+
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { name, value } = e.target
+    setForm({ ...form, [name]: value })
+  }
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    if (!form.oldPassword.trim() || !form.newPassword.trim()) return
+    mutateChangePassword(form)
+  }
+
+  const askUnregister = () => {
+    /* open Dialog */
+  }
+
+  // security code
+  if (!user) return null
   return (
     <Block>
       <div>
+        <Title>내 계정</Title>
+
         <Section>
           <h4>아이디</h4>
-
           <Username>{user.username}</Username>
         </Section>
 
         <Section>
           <h4>비밀번호</h4>
+          <form onSubmit={onSubmit}>
+            <InputGroup>
+              <Input
+                name="oldPassword"
+                placeholder="현재 비밀번호"
+                type="password"
+                onChange={onChange}
+                value={form.oldPassword}
+              />
 
-          <InputGroup>
-            <Input placeholder="현재 비밀번호" type="password" />
-            <Input placeholder="새 비밀번호 비밀번호" type="password" />
-          </InputGroup>
+              <Input
+                name="newPassword"
+                placeholder="새 비밀번호"
+                type="password"
+                onChange={onChange}
+                value={form.newPassword}
+              />
+            </InputGroup>
 
-          <VariantLinkOrButton variant="primary">
-            비밀번호 변경
-          </VariantLinkOrButton>
+            <VariantLinkOrButton variant="primary" type="submit">
+              비밀번호 변경
+            </VariantLinkOrButton>
+          </form>
         </Section>
       </div>
-      <div>
-        <VariantLinkOrButton variant="secondary" layout="fullWidth">
+
+      <UnregisterWrapper>
+        <VariantLinkOrButton variant="secondary" onClick={askUnregister}>
           계정 탈퇴
         </VariantLinkOrButton>
-      </div>
+      </UnregisterWrapper>
     </Block>
   )
 }
 export default AccountSetting
 
 // Inner Components
+
+const Title = styled.h1`
+  ${Font.style()
+    .size(48)
+    .weight(800)
+    .color(globalColors.grey5)
+    .lineHeight(1.5)
+    .create()};
+  margin-top: 0;
+  margin-bottom: 32px;
+`
 
 const Block = styled.div`
   ${Flex.item().flex(1).create()};
@@ -54,6 +131,14 @@ const Block = styled.div`
     .justifyContent('space-between')
     .create()};
   padding: 16px;
+  ${Media.minWidth.mobile} {
+    ${Flex.item().flex('initial').create()};
+    width: 100%;
+    max-width: 768px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 96px;
+  }
 `
 
 const Section = styled.section`
@@ -65,6 +150,9 @@ const Section = styled.section`
   & + & {
     margin-top: 32px;
   }
+  ${Media.minWidth.mobile} {
+    width: 460px;
+  }
 `
 
 const Username = styled.div`
@@ -75,4 +163,10 @@ const InputGroup = styled.div`
   ${Flex.container().direction('column').create()};
   gap: 8px;
   margin-bottom: 8px;
+`
+
+const UnregisterWrapper = styled.div`
+  ${Media.minWidth.mobile} {
+    margin-top: 96px;
+  }
 `
