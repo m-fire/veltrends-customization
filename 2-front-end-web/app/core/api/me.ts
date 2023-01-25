f78136c80248a3b3db8f0815209670c45cbad277
countless-exercise <truestarmello@gmail.com>
2023-01-25 17:45:52 +0900

    feat core/api/me.ts: 사용자계정 API함수 추가: chagnePassword(..), unregister()

--- 2-front-end-web/app/core/api/me.ts
+++ 2-front-end-web/app/core/api/me.ts
@@ -2,11 +2,26 @@ import { client } from '~/common/api/client'
 import { UserInfo } from '~/common/api/types'
 
 const URL_ME = '/api/me' as const
+const URL_ME_CHANGE_PASSWORD = '/api/me/change-password' as const
 
 export class UserInformation {
   static async getUserInfo() {
     const response = await client.get<UserInfo>(URL_ME)
     if (!response.data) return null
+    return response.data
+  }
+
+  static async changePassword(params: {
+    oldPassword: string
+    newPassword: string
+  }) {
+    const response = await client.post<UserInfo>(URL_ME_CHANGE_PASSWORD, params)
+    if (!response.data) return null
+    return response.data
+  }
+
+  static async unregister() {
+    const response = await client.delete(URL_ME)
     return response.data
   }
 }
